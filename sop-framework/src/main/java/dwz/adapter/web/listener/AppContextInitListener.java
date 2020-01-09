5a4ca3fd3bf8a63f98ccd847ba4715fdeaa325a2
masonleung2016 <masonleung@163.com>
2020-01-09 14:15:48 +0800

    Create AppContextInitListener.java

---
+++ sop-framework/src/main/java/dwz/adapter/web/listener/AppContextInitListener.java
@@ -0,0 +1,40 @@
+package dwz.adapter.web.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 11:37
+ * @Package: dwz.adapter.web.listener
+ */
+
+public class AppContextInitListener implements ServletContextListener,
+        HttpSessionListener {
+
+    private static final String ETC_FILE = "etc_file";
+
+    public AppContextInitListener() {
+    }
+
+    public void contextDestroyed(ServletContextEvent event) {
+
+    }
+
+    public void contextInitialized(ServletContextEvent event) {
+        ServletContext context = event.getServletContext();
+        String configFile = context.getInitParameter(ETC_FILE);
+    }
+
+    public void sessionCreated(HttpSessionEvent event) {
+    }
+
+    public void sessionDestroyed(HttpSessionEvent event) {
+        HttpSession session = event.getSession();
+        session.invalidate();
+    }
+}
