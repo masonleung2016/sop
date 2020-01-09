893ca7620ebf519db906965881fd2a755149a04f
masonleung2016 <masonleung@163.com>
2020-01-09 14:16:43 +0800

    Create PageTarget.java

---
+++ sop-framework/src/main/java/dwz/business/enums/PageTarget.java
@@ -0,0 +1,28 @@
+package dwz.business.enums;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 11:39
+ * @Package: dwz.business.enums
+ */
+
+public enum PageTarget {
+    header("Header"),
+    //	sidebar("Sidebar"),
+    footer("Footer");
+
+    private String name;
+
+    PageTarget(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return this.toString();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
