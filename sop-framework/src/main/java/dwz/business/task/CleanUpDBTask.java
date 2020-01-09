3919a6dea0d5d8398ae02782aeff84f1ae728585
masonleung2016 <masonleung@163.com>
2020-01-09 14:17:27 +0800

    Create CleanUpDBTask.java

---
+++ sop-framework/src/main/java/dwz/business/task/CleanUpDBTask.java
@@ -0,0 +1,15 @@
+package dwz.business.task;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 11:40
+ * @Package: dwz.business.task
+ */
+
+public class CleanUpDBTask implements Runnable {
+
+    public void run() {
+        System.out.println("Start CleanUpDBTask...");
+    }
+
+}
