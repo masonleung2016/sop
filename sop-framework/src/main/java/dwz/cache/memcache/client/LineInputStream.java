02cb79fc2d9753db0de2d9fbfc31ab4485e81f8c
masonleung2016 <masonleung@163.com>
2020-01-09 14:19:12 +0800

    Create LineInputStream.java

---
+++ sop-framework/src/main/java/dwz/cache/memcache/client/LineInputStream.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2008 Greg Whalin
+ * All rights reserved.
+ * <p>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the BSD license
+ * <p>
+ * This library is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.
+ * <p>
+ * You should have received a copy of the BSD License along with this
+ * library.
+ *
+ * @author greg whalin <greg@meetup.com>
+ */
+
+package dwz.cache.memcache.client;
+
+import java.io.IOException;
+
+public interface LineInputStream {
+
+    /**
+     * Read everything up to the next end-of-line.  Does
+     * not include the end of line, though it is consumed
+     * from the input.
+     * @return All next up to the next end of line.
+     */
+    public String readLine() throws IOException;
+
+    /**
+     * Read everything up to and including the end of line.
+     */
+    public void clearEOL() throws IOException;
+
+    /**
+     * Read some bytes.
+     * @param buf   The buffer into which read.
+     * @return The number of bytes actually read, or -1 if none could be read.
+     */
+    public int read(byte[] buf) throws IOException;
+}
