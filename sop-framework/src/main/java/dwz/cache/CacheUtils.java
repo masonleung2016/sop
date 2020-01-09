7bdd81819922a7b6b069ce361a3f8f6560e53e55
masonleung2016 <masonleung@163.com>
2020-01-09 14:22:20 +0800

    Create CacheUtils.java

---
+++ sop-framework/src/main/java/dwz/cache/CacheUtils.java
@@ -0,0 +1,63 @@
+package dwz.cache;
+
+import java.io.Serializable;
+
+import org.springframework.util.Assert;
+
+/**
+ * <strong>CacheUtils</strong><br>
+ * <br>
+ * <strong>Create on : 2011-12-13<br></strong>
+ * <p>
+ * <strong>Copyright (C) Ecointel Software Co.,Ltd.<br></strong>
+ * <p>
+ *
+ * @author peng.shi peng.shi@ecointel.com.cn<br>
+ * @version <strong>Ecointel v1.0.0</strong><br>
+ */
+
+public class CacheUtils {
+
+    /**
+     * 通过Class得到Cache Key
+     *
+     * @param clazz
+     * @return
+     */
+    @SuppressWarnings("rawtypes")
+    public static String keyOfClassPrefix(Class clazz) {
+        Assert.notNull(clazz);
+        return clazz.getName();
+    }
+
+    /**
+     * 通过Class获取CacheKey
+     *
+     * @param clazz
+     * @param id
+     * @return
+     */
+    @SuppressWarnings("rawtypes")
+    public static String keyOfClass(Class clazz, Serializable id) {
+        Assert.notNull(clazz);
+        Assert.notNull(id);
+        return clazz.getName() + "_" + id;
+    }
+
+    /**
+     * 通过Class以及Serializable得到Cache Key
+     *
+     * @param clazz
+     * @param id
+     * @return
+     */
+    public static String keyOfObject(Object object, Serializable id) {
+        Assert.notNull(object);
+        Assert.notNull(id);
+        return object.getClass().getName() + "_" + id;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(keyOfClassPrefix(CacheManager.class));
+    }
+}
