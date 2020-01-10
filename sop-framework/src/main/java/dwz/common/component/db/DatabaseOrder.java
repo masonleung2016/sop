758247731e450e71170b4ea85e29731cf7e16dde
masonleung2016 <masonleung@163.com>
2020-01-10 09:25:50 +0800

    Create DatabaseOrder.java

---
+++ sop-framework/src/main/java/dwz/common/component/db/DatabaseOrder.java
@@ -0,0 +1,13 @@
+package dwz.common.component.db;
+
+/**
+ * 数据库排序关键字
+ *
+ * @Author: LCF
+ * @Date: 2020/1/8 15:07
+ * @Package: dwz.common.component.db
+ */
+
+public enum DatabaseOrder {
+    asc, desc
+}
