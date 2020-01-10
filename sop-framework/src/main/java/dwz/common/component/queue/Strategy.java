e4c7535c83f05bd29751bb140252319bff24a7c4
masonleung2016 <masonleung@163.com>
2020-01-10 09:28:26 +0800

    Create Strategy.java

---
+++ sop-framework/src/main/java/dwz/common/component/queue/Strategy.java
@@ -0,0 +1,12 @@
+package dwz.common.component.queue;
+
+/**
+ * 策略抽象对象
+ *
+ * @Author: LCF
+ * @Date: 2020/1/8 15:15
+ * @Package: dwz.common.component.queue
+ */
+public abstract class Strategy implements PutMethod, GetMethod {
+
+}
