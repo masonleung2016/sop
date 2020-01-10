fed5a414f7b7efbceada88d018f6c60169a09d07
masonleung2016 <masonleung@163.com>
2020-01-10 09:29:05 +0800

    Create ThreadPoolDiagnose.java

---
+++ sop-framework/src/main/java/dwz/common/component/threadpool/ThreadPoolDiagnose.java
@@ -0,0 +1,22 @@
+package dwz.common.component.threadpool;
+
+import dwz.common.component.threadpool.ThreadPool.ThreadDiagnose;
+
+/**
+ * 诊断信息接口
+ *
+ * @Author: LCF
+ * @Date: 2020/1/8 15:17
+ * @Package: dwz.common.component.threadpool
+ */
+public interface ThreadPoolDiagnose {
+
+    public boolean running();
+
+    public int maxsize();
+
+    public int size();
+
+    public ThreadDiagnose[] getDiagnose();
+
+}
