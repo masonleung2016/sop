0e784964602cdbde13d7abcff08cf33536e3b9a2
masonleung2016 <masonleung@163.com>
2020-01-10 09:41:24 +0800

    Create MapUtils.java

---
+++ sop-framework/src/main/java/dwz/common/util/MapUtils.java
@@ -0,0 +1,22 @@
+package dwz.common.util;
+
+import java.util.Map;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:03
+ * @Package: dwz.common.util
+ */
+public class MapUtils {
+    @SuppressWarnings("all")
+    public static void putIfNull(Map map, Object key, Object defaultValue) {
+        if (key == null)
+            throw new IllegalArgumentException("key must be not null");
+        if (map == null)
+            throw new IllegalArgumentException("map must be not null");
+        if (map.get(key) == null) {
+            map.put(key, defaultValue);
+        }
+    }
+
+}
