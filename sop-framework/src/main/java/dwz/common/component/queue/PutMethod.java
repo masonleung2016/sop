3ebea699b7e5eb8f21929b8d750755d764320e77
masonleung2016 <masonleung@163.com>
2020-01-10 09:28:03 +0800

    Create PutMethod.java

---
+++ sop-framework/src/main/java/dwz/common/component/queue/PutMethod.java
@@ -0,0 +1,14 @@
+package dwz.common.component.queue;
+
+/**
+ * 入队方式
+ *
+ * @Author: LCF
+ * @Date: 2020/1/8 15:13
+ * @Package: dwz.common.component.queue
+ */
+public interface PutMethod {
+
+    public boolean allowPut(BaseQueue<Object> queue, Object o);
+
+}
