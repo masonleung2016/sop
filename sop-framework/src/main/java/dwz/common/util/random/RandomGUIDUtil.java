5d27b9d50c70ba2698ea6a2de62830bd829bd7d0
masonleung2016 <masonleung@163.com>
2020-01-10 09:33:23 +0800

    Create RandomGUIDUtil.java

---
+++ sop-framework/src/main/java/dwz/common/util/random/RandomGUIDUtil.java
@@ -0,0 +1,19 @@
+package dwz.common.util.random;
+
+/**
+ * Created on 2007-1-11
+ * <p>
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+public class RandomGUIDUtil {
+    /**
+     * 产生唯一的随机字符串
+     *
+     * @return
+     */
+    public static String generateKey() {
+        return new RandomGUID(true).toString();
+    }
+}
