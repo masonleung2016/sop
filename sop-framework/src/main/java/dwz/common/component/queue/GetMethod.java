bc259b56595fbf92bdb551c8a2ef30b7a7889b84
masonleung2016 <masonleung@163.com>
2020-01-10 09:27:41 +0800

    Create GetMethod.java

---
+++ sop-framework/src/main/java/dwz/common/component/queue/GetMethod.java
@@ -0,0 +1,13 @@
+package dwz.common.component.queue;
+
+/**
+ * 出队方式(用于扩展)
+ *
+ * @Author: LCF
+ * @Date: 2020/1/8 15:12
+ * @Package: dwz.common.component.queue
+ */
+public interface GetMethod {
+
+}
+
