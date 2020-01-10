8c4b67f748e58bab615c80d5d21506d6b49da1c8
masonleung2016 <masonleung@163.com>
2020-01-10 09:45:25 +0800

    Create MailTemplateKey.java

---
+++ sop-framework/src/main/java/dwz/common/MailTemplateKey.java
@@ -0,0 +1,11 @@
+package dwz.common;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:16
+ * @Package: dwz.common
+ */
+
+public enum MailTemplateKey {
+    defaultVm, verifyEmail, registerConfirm, emailToFriend, forgetPassword
+}
