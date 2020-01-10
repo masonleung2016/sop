095f8ddccbcb4cb51958ee93c10c63d6e926f7c8
masonleung2016 <masonleung@163.com>
2020-01-10 09:43:41 +0800

    Create ServerInfo.java

---
+++ sop-framework/src/main/java/dwz/common/util/ServerInfo.java
@@ -0,0 +1,20 @@
+package dwz.common.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:12
+ * @Package: dwz.common.util
+ */
+
+public class ServerInfo {
+    public static boolean isAjax(HttpServletRequest request) {
+        if (request != null
+                && "XMLHttpRequest".equalsIgnoreCase(request
+                .getHeader("X-Requested-With")))
+            return true;
+        return false;
+    }
+
+}
