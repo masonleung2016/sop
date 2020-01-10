6b3cac722df56e537350bdea46c0b450bb360929
masonleung2016 <masonleung@163.com>
2020-01-10 09:26:59 +0800

    Create AdvanceQueue.java

---
+++ sop-framework/src/main/java/dwz/common/component/queue/AdvanceQueue.java
@@ -0,0 +1,21 @@
+package dwz.common.component.queue;
+
+/**
+ * 队列高级行为
+ * (此行为仅暴露给业务中实际操作入队和出队行为的用户)
+ *
+ * @Author: LCF
+ * @Date: 2020/1/8 15:09
+ * @Package: dwz.common.component.queue
+ */
+public interface AdvanceQueue<E> {
+
+    public void putInStrategy(E obj) throws Exception;
+
+    public void putInStrategy(E obj, long maxlimit) throws Exception;
+
+    public Object getInStrategy() throws Exception;
+
+    public Object getInStrategy(long maxlimit) throws Exception;
+
+}
