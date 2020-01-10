ce0ad90705e879665ac7a3075cc9589b0dfb14f6
masonleung2016 <masonleung@163.com>
2020-01-10 09:34:10 +0800

    Create ResourceDetailsService.java

---
+++ sop-framework/src/main/java/dwz/common/util/security/springsecurity/ResourceDetailsService.java
@@ -0,0 +1,17 @@
+package dwz.common.util.security.springsecurity;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 15:40
+ * @Package: dwz.common.util.security.springsecurity
+ */
+
+public interface ResourceDetailsService {
+
+    /**
+     * 返回带顺序的URL-授权关系Map, key为受保护的URL, value为能访问该URL的授权名称列表,以','分隔.
+     */
+    public LinkedHashMap<String, String> getRequestMap() throws Exception; //NOSONAR
+}
