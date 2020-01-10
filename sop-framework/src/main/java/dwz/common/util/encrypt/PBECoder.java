872395bc7e905232da36485b33670ccdd4d64de2
masonleung2016 <masonleung@163.com>
2020-01-10 09:30:17 +0800

    Create PBECoder.java

---
+++ sop-framework/src/main/java/dwz/common/util/encrypt/PBECoder.java
@@ -0,0 +1,133 @@
+package dwz.common.util.encrypt;
+
+import java.security.Key;
+import java.util.Random;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 15:30
+ * @Package: dwz.common.util.encrypt
+ */
+
+public class PBECoder {
+    /**
+     * 支持以下任意一种算法
+     * <p>
+     * < pre>
+     * PBEWithMD5AndDES
+     * PBEWithMD5AndTripleDES
+     * PBEWithSHA1AndDESede
+     * PBEWithSHA1AndRC2_40
+     * < /pre>
+     */
+//    	暂时出错，报invalid key size，要换包
+//        public static final String ALGORITHM = "PBEWithMD5AndTripleDES";
+    public static final String ALGORITHM = "PBEWithMD5AndDES";
+
+    public static final byte[] SALT = new byte[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+    /**
+     * 盐初始化
+     *
+     * @return
+     * @throws Exception
+     */
+    public static byte[] initSalt() throws Exception {
+        byte[] salt = new byte[8];
+        Random random = new Random();
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    /**
+     * 转换密钥< br>
+     *
+     * @param password
+     * @return
+     * @throws Exception
+     */
+    private static Key toKey(String password) throws Exception {
+        PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray());
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(ALGORITHM);
+        SecretKey secretKey = keyFactory.generateSecret(keySpec);
+
+        return secretKey;
+    }
+
+    /**
+     * 加密
+     *
+     * @param data     数据
+     * @param password 密码
+     * @param salt     盐
+     * @return
+     * @throws Exception
+     */
+    public static byte[] encrypt(byte[] data, String password, byte[] salt)
+            throws Exception {
+
+        Key key = toKey(password);
+
+        PBEParameterSpec paramSpec = new PBEParameterSpec(salt, 100);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
+
+        return cipher.doFinal(data);
+
+    }
+
+    public static byte[] encrypt(byte[] data, String password)
+            throws Exception {
+
+        Key key = toKey(password);
+
+        PBEParameterSpec paramSpec = new PBEParameterSpec(SALT, 100);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key, paramSpec);
+
+        return cipher.doFinal(data);
+
+    }
+
+
+    /**
+     * 解密
+     *
+     * @param data     数据
+     * @param password 密码
+     * @param salt     盐
+     * @return
+     * @throws Exception
+     */
+    public static byte[] decrypt(byte[] data, String password, byte[] salt)
+            throws Exception {
+
+        Key key = toKey(password);
+
+        PBEParameterSpec paramSpec = new PBEParameterSpec(salt, 100);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
+
+        return cipher.doFinal(data);
+
+    }
+
+    public static byte[] decrypt(byte[] data, String password)
+            throws Exception {
+
+        Key key = toKey(password);
+
+        PBEParameterSpec paramSpec = new PBEParameterSpec(SALT, 100);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key, paramSpec);
+
+        return cipher.doFinal(data);
+
+    }
+}
