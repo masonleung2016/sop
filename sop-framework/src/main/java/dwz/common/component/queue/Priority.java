39de5282660a4c7f32b70e00ce1c85e58a2cd19e
masonleung2016 <masonleung@163.com>
2020-01-10 09:27:52 +0800

    Create Priority.java

---
+++ sop-framework/src/main/java/dwz/common/component/queue/Priority.java
@@ -0,0 +1,15 @@
+package dwz.common.component.queue;
+
+/**
+ * 优先级接口
+ * 若要实现优先级队列，请将要入队的对象实现此接口，
+ * 并实现compareTo方法来定制自己的比较策略
+ *
+ * @Author: LCF
+ * @Date: 2020/1/8 15:12
+ * @Package: dwz.common.component.queue
+ */
+
+public interface Priority extends Comparable<Priority> {
+
+}
