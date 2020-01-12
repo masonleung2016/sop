d2f81b8bea2074a7d1559c7a7062b2854517cf85
masonleung2016 <masonleung@163.com>
2020-01-12 11:34:28 +0800

    Create TaskParse.java

---
+++ sop-framework/src/main/java/dwz/framework/timer/TaskParse.java
@@ -0,0 +1,17 @@
+package dwz.framework.timer;
+
+import java.util.Collection;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:11
+ * @Package: dwz.framework.timer
+ */
+
+public interface TaskParse {
+
+    Collection<TaskUnit> getTaskUnits();
+
+    TaskUnit getTaskUnit(String name);
+
+}
