3ad1d2a2e7e7cc6bf7b2ea1fb8b5b2266ed9202e
masonleung2016 <masonleung@163.com>
2020-01-12 11:52:24 +0800

    Create JrTxtFileGenerator.java

---
+++ sop-framework/src/main/java/sop/filegen/generator/impl/JrTxtFileGenerator.java
@@ -0,0 +1,47 @@
+package sop.filegen.generator.impl;
+
+import java.util.List;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import sop.filegen.FileGenerationException;
+import sop.filegen.FileGeneratorConstant;
+import sop.filegen.GenRequest;
+import sop.filegen.GenResult;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:55
+ * @Package: sop.filegen.generator.impl
+ */
+
+public class JrTxtFileGenerator extends BaseFileGenerator {
+
+    @Override
+    protected GenResult generateFile(GenRequest request) throws FileGenerationException {
+
+        FileGeneratorUtils.makeDirIfNotExists(getOutputFolder());
+
+        GenResult result = new GenResult();
+        try {
+            final String reportFileName = FileGeneratorUtils.genReportFileName(request);
+            String outFile = FileGeneratorUtils.getReportOutputFullPath(getOutputFolder(), reportFileName, ".csv");
+
+            List<Object> dataList = request.getDataList();
+            JRDataSource jrds = new JRBeanCollectionDataSource(dataList);
+            outFile = JrReportEngine.getInstance().exportTxtWithJRDatasource(request.getReportId(), FileGeneratorUtils.getParamMap(request), jrds, outFile);
+            result.setReturnCode(FileGeneratorConstant.SUCCESS);
+            result.setReportFile(outFile);
+
+        } catch (Exception e) {
+            logger.error("TXT generation error:", e);
+            throw new FileGenerationException(e.getMessage());
+        } finally {
+            result.setReportId(request.getReportId());
+            result.setStatus(FileGeneratorConstant.REPORT_STATUS_COMPLETED);
+        }
+        return result;
+    }
+
+
+}
