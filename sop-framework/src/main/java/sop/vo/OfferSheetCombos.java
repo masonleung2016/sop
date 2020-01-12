ef045278d8c230b4641776f94490d043dedb90f9
masonleung2016 <masonleung@163.com>
2020-01-12 12:41:26 +0800

    Create OfferSheetCombos.java

---
+++ sop-framework/src/main/java/sop/vo/OfferSheetCombos.java
@@ -0,0 +1,42 @@
+package sop.vo;
+
+import java.util.List;
+
+import sop.persistence.beans.CustAttn;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 11:42
+ * @Package: sop.vo
+ */
+
+
+public class OfferSheetCombos {
+    private List<TxnVo> txns;
+    private List<CustomerVo> customers;
+    private List<CustAttn> custAttns;
+
+    public List<TxnVo> getTxns() {
+        return txns;
+    }
+
+    public void setTxns(List<TxnVo> txns) {
+        this.txns = txns;
+    }
+
+    public List<CustomerVo> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<CustomerVo> customers) {
+        this.customers = customers;
+    }
+
+    public List<CustAttn> getCustAttns() {
+        return custAttns;
+    }
+
+    public void setCustAttns(List<CustAttn> custAttns) {
+        this.custAttns = custAttns;
+    }
+}
