fdfe4d3d88afb0a0a3b978e8f3f8a1c3d1c1bcf5
masonleung2016 <masonleung@163.com>
2020-01-12 12:31:54 +0800

    Create StaffServiceMgr.java

---
+++ sop-framework/src/main/java/sop/services/StaffServiceMgr.java
@@ -0,0 +1,33 @@
+package sop.services;
+
+import java.util.List;
+
+import dwz.framework.sys.business.BusinessObjectServiceMgr;
+import dwz.framework.vo.Checker;
+import dwz.persistence.BaseConditionVO;
+import sop.persistence.beans.Staff;
+import sop.vo.StaffVo;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 11:13
+ * @Package: sop.services
+ */
+
+public interface StaffServiceMgr extends BusinessObjectServiceMgr {
+    String SERVICE_NAME = "staffServiceMgr";
+
+    List<StaffVo> getStaffListByCondition(BaseConditionVO vo);
+
+    void getStaffListNum(BaseConditionVO vo);
+
+    Staff getStaffById(String fkSfCode);
+
+    boolean updateStaff(Staff currentStaff);
+
+    Checker checkStaff(Staff currentStaff);
+
+    boolean insertStaff(Staff currentStaff);
+
+    boolean deleteStaff(String fkSfCode);
+}
