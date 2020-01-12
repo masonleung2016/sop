361be7c5216bcaa8ef53b9bbdcedadb406657135
masonleung2016 <masonleung@163.com>
2020-01-13 00:24:31 +0800

    Create SoSearchItemConditionVo.java

---
+++ sop-framework/src/main/java/sop/vo/SoSearchItemConditionVo.java
@@ -0,0 +1,32 @@
+package sop.vo;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 12:39
+ * @Package: sop.vo
+ */
+
+public class SoSearchItemConditionVo extends OffSearchItemConditionVo {
+    /**
+     * search list condition
+     */
+    private String fromSoNo;
+
+    private String toSoNo;
+
+    public String getFromSoNo() {
+        return fromSoNo;
+    }
+
+    public void setFromSoNo(String fromSoNo) {
+        this.fromSoNo = fromSoNo;
+    }
+
+    public String getToSoNo() {
+        return toSoNo;
+    }
+
+    public void setToSoNo(String toSoNo) {
+        this.toSoNo = toSoNo;
+    }
+}
