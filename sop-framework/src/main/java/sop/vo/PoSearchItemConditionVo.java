cb821abd81562dd31aed43fe7f32d5010c15a751
masonleung2016 <masonleung@163.com>
2020-01-12 12:43:12 +0800

    Create PoSearchItemConditionVo.java

---
+++ sop-framework/src/main/java/sop/vo/PoSearchItemConditionVo.java
@@ -0,0 +1,32 @@
+package sop.vo;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 11:46
+ * @Package: sop.vo
+ */
+
+public class PoSearchItemConditionVo extends SoSearchItemConditionVo {
+    /**
+     * search list condition
+     */
+    private String fromPoNo;
+
+    private String toPoNo;
+
+    public String getFromPoNo() {
+        return fromPoNo;
+    }
+
+    public void setFromPoNo(String fromPoNo) {
+        this.fromPoNo = fromPoNo;
+    }
+
+    public String getToPoNo() {
+        return toPoNo;
+    }
+
+    public void setToPoNo(String toPoNo) {
+        this.toPoNo = toPoNo;
+    }
+}
