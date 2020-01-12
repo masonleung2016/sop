067d27bd8660bd996094fe9aa780c2363fba0ec7
masonleung2016 <masonleung@163.com>
2020-01-13 00:24:49 +0800

    Create StaffVo.java

---
+++ sop-framework/src/main/java/sop/vo/StaffVo.java
@@ -0,0 +1,42 @@
+package sop.vo;
+
+import it.sauronsoftware.base64.Base64;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 12:39
+ * @Package: sop.vo
+ */
+
+public class StaffVo {
+    private String sfCode;
+
+    private String encodeSfCode;
+
+    private String sfName;
+
+    public String getSfCode() {
+        return sfCode;
+    }
+
+    public void setSfCode(String sfCode) {
+        this.sfCode = sfCode;
+        this.encodeSfCode = Base64.encode(sfCode, "utf-8");
+    }
+
+    public String getSfName() {
+        return sfName;
+    }
+
+    public void setSfName(String sfName) {
+        this.sfName = sfName;
+    }
+
+    public String getEncodeSfCode() {
+        return encodeSfCode;
+    }
+
+    public void setEncodeSfCode(String encodeSfCode) {
+        this.encodeSfCode = encodeSfCode;
+    }
+}
