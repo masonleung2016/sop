8d4c5a4596625499b2321fb8d6a2933826ed183d
masonleung2016 <masonleung@163.com>
2020-01-12 11:31:00 +0800

    Create PagerNextTag.java

---
+++ sop-framework/src/main/java/dwz/framework/taglib/PagerNextTag.java
@@ -0,0 +1,56 @@
+package dwz.framework.taglib;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:04
+ * @Package: dwz.framework.taglib
+ */
+public class PagerNextTag extends TagSupport {
+
+    private static final long serialVersionUID = 2585520376304429512L;
+    private int lastPage;
+    private int nextPage;
+    private int totaItems;
+    private int numPerPage;
+    private int currentPageNum;
+
+    public int doStartTag() throws JspException {
+        PagerInfoTag tag = (PagerInfoTag) findAncestorWithClass(this, PagerInfoTag.class);
+        if (tag == null) {
+            throw new JspTagException("The root tag not found!");
+        }
+        try {
+            totaItems = Integer.parseInt(tag.getTotalItems());
+            numPerPage = Integer.parseInt(tag.getNumPerPage());
+            currentPageNum = Integer.parseInt(tag.getCurrentPageNum());
+        } catch (Exception e) {
+            throw new JspException(e);
+        }
+        lastPage = totaItems / numPerPage;
+        if (totaItems % numPerPage != 0) {
+            lastPage++;
+        }
+
+        if (currentPageNum < lastPage)
+            nextPage = currentPageNum + 1;
+        else
+            nextPage = lastPage;
+
+        if (currentPageNum == lastPage) {
+            return SKIP_BODY;
+        }
+
+        pageContext.setAttribute("nextPage", nextPage);
+        return EVAL_BODY_INCLUDE;
+    }
+
+    public int doEndTag() throws JspException {
+        pageContext.removeAttribute("nextPage");
+        return EVAL_PAGE;
+    }
+
+}
