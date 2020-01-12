c3278a1e299c948f6ab00fe43892e6f54a194ec0
masonleung2016 <masonleung@163.com>
2020-01-12 11:22:13 +0800

    Create Role.java

---
+++ sop-framework/src/main/java/dwz/framework/enums/Role.java
@@ -0,0 +1,26 @@
+package dwz.framework.enums;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:39
+ * @Package: dwz.framework.enums
+ */
+
+public enum Role {
+    admin("管理员"), normalUser("普通用户");
+
+    private String name;
+
+    private Role(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
