b91efaa0de73185d136e5128c5c3b70a939534e5
masonleung2016 <masonleung@163.com>
2020-01-12 11:24:18 +0800

    Create DefaultMessageConverter.java

---
+++ sop-framework/src/main/java/dwz/framework/jms/DefaultMessageConverter.java
@@ -0,0 +1,24 @@
+package dwz.framework.jms;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:43
+ * @Package: dwz.framework.jms
+ */
+public class DefaultMessageConverter {
+
+    public Message toMessage(Object obj, Session session) throws JMSException {
+
+        return null;
+    }
+
+
+    public Object fromMessage(Message msg) throws JMSException {
+
+        return null;
+    }
+}
