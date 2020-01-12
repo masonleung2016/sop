e54b5bf7234314f18399f2ff02f7e8f0b755c274
masonleung2016 <masonleung@163.com>
2020-01-12 12:04:55 +0800

    Create ItemType.java

---
+++ sop-framework/src/main/java/sop/persistence/beans/ItemType.java
@@ -0,0 +1,33 @@
+package sop.persistence.beans;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 9:50
+ * @Package: sop.persistence.beans
+ */
+
+public class ItemType extends BaseBean {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
