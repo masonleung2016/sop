42bb86a21f7f1d6125e756714a038d6b8a2e092b
masonleung2016 <masonleung@163.com>
2020-01-12 11:33:58 +0800

    Create TaskFactory.java

---
+++ sop-framework/src/main/java/dwz/framework/timer/TaskFactory.java
@@ -0,0 +1,49 @@
+package dwz.framework.timer;
+
+import java.util.Collection;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:11
+ * @Package: dwz.framework.timer
+ */
+
+public abstract class TaskFactory {
+
+    private static final String className = "dwz.framework.timer.impl.DefaultTaskFactory";
+
+    private static TaskFactory factory = null;
+
+    private static Object lock = new Object();
+
+    public static TaskFactory getFactory() {
+        if (factory == null) {
+            synchronized (lock) {
+                if (factory == null) {
+                    try {
+                        Class<?> clazz = Class.forName(className);
+                        factory = (TaskFactory) clazz.newInstance();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (InstantiationException e) {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return factory;
+    }
+
+    public abstract void initTasks(String filePath);
+
+    public abstract Runnable getTask(String name);
+
+    public abstract void startAllTasks();
+
+    public abstract void startTasks(Collection<TaskUnit> units);
+
+    public abstract TaskParse getTaskParse();
+
+}
