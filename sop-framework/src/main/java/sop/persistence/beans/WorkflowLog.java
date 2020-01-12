61e419d75c7ca721be6ab8b89266a60817ea58be
masonleung2016 <masonleung@163.com>
2020-01-12 12:13:02 +0800

    Create WorkflowLog.java

---
+++ sop-framework/src/main/java/sop/persistence/beans/WorkflowLog.java
@@ -0,0 +1,41 @@
+package sop.persistence.beans;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 10:10
+ * @Package: sop.persistence.beans
+ */
+
+public class WorkflowLog extends BaseBean {
+
+    private Integer id;
+
+    private String qcNo;
+
+
+    private String comment;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getQcNo() {
+        return qcNo;
+    }
+
+    public void setQcNo(String qcNo) {
+        this.qcNo = qcNo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
