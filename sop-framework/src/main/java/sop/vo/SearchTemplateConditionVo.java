71bca455cb469889270344a7c313594da9270fe0
masonleung2016 <masonleung@163.com>
2020-01-13 00:22:39 +0800

    Create SearchTemplateConditionVo.java

---
+++ sop-framework/src/main/java/sop/vo/SearchTemplateConditionVo.java
@@ -0,0 +1,57 @@
+package sop.vo;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 12:37
+ * @Package: sop.vo
+ */
+
+public class SearchTemplateConditionVo {
+    private Integer fkItemMaster;
+    private String itemMasterType;
+    private Integer fkTemplate;
+    private String templateName;
+    private String engTemplateName;
+
+    public String getItemMasterType() {
+        return itemMasterType;
+    }
+
+    public void setItemMasterType(String itemMasterType) {
+        this.itemMasterType = itemMasterType;
+    }
+
+    public String getTemplateName() {
+        return templateName;
+    }
+
+    public void setTemplateName(String templateName) {
+        this.templateName = templateName;
+    }
+
+    public Integer getFkItemMaster() {
+        return fkItemMaster;
+    }
+
+    public void setFkItemMaster(Integer fkItemMaster) {
+        this.fkItemMaster = fkItemMaster;
+    }
+
+    public Integer getFkTemplate() {
+        return fkTemplate;
+    }
+
+    public void setFkTemplate(Integer fkTemplate) {
+        this.fkTemplate = fkTemplate;
+    }
+
+    public String getEngTemplateName() {
+        return engTemplateName;
+    }
+
+    public void setEngTemplateName(String engTemplateName) {
+        this.engTemplateName = engTemplateName;
+    }
+
+
+}
