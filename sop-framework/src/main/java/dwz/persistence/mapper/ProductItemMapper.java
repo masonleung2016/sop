056149b485149fcb0fd0e567ef5861a26c513dcf
masonleung2016 <masonleung@163.com>
2020-01-12 11:41:31 +0800

    Create ProductItemMapper.java

---
+++ sop-framework/src/main/java/dwz/persistence/mapper/ProductItemMapper.java
@@ -0,0 +1,25 @@
+package dwz.persistence.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.stereotype.Repository;
+
+import dwz.dal.BaseMapper;
+import dwz.persistence.BaseConditionVO;
+import sop.persistence.beans.Order;
+import sop.persistence.beans.ProductItem;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:28
+ * @Package: dwz.persistence.mapper
+ */
+
+@Repository
+public interface ProductItemMapper extends BaseMapper<Order, Integer> {
+
+    List<ProductItem> findPageBreakByCondition(BaseConditionVO vo, RowBounds rb);
+
+    Integer findNumberByCondition(BaseConditionVO vo);
+}
