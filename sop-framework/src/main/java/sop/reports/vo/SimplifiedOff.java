af1d312eb6de82370b1911959c9ca926e5cbc41c
masonleung2016 <masonleung@163.com>
2020-01-12 12:19:26 +0800

    Create SimplifiedOff.java

---
+++ sop-framework/src/main/java/sop/reports/vo/SimplifiedOff.java
@@ -0,0 +1,185 @@
+package sop.reports.vo;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 10:27
+ * @Package: sop.reports.vo
+ */
+
+
+public class SimplifiedOff {
+    private String no;
+    private String date;
+    private String vto;
+    private String attn;
+    private String code;
+    private BigDecimal price;
+    private String fob;
+    private String itno;
+    private String itname;
+    private BigDecimal it_pkg_20_qty;
+    private BigDecimal it_pkg_40_qty;
+    private BigDecimal it_pkg_40HQ_qty;
+    private String off2_desc_offer_sh;
+    private BigDecimal cbm;
+    private String it_uom;
+    private String it_pkg_exp_pcs;
+    private String it_image_1;
+    private String it_pkg_detail;
+    private String it_desc_of_sh;
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getVto() {
+        return vto;
+    }
+
+    public void setVto(String vto) {
+        this.vto = vto;
+    }
+
+    public String getAttn() {
+        return attn;
+    }
+
+    public void setAttn(String attn) {
+        this.attn = attn;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getFob() {
+        return fob;
+    }
+
+    public void setFob(String fob) {
+        this.fob = fob;
+    }
+
+    public String getItno() {
+        return itno;
+    }
+
+    public void setItno(String itno) {
+        this.itno = itno;
+    }
+
+    public String getItname() {
+        return itname;
+    }
+
+    public void setItname(String itname) {
+        this.itname = itname;
+    }
+
+    public BigDecimal getIt_pkg_20_qty() {
+        return it_pkg_20_qty;
+    }
+
+    public void setIt_pkg_20_qty(BigDecimal it_pkg_20_qty) {
+        this.it_pkg_20_qty = it_pkg_20_qty;
+    }
+
+    public BigDecimal getIt_pkg_40_qty() {
+        return it_pkg_40_qty;
+    }
+
+    public void setIt_pkg_40_qty(BigDecimal it_pkg_40_qty) {
+        this.it_pkg_40_qty = it_pkg_40_qty;
+    }
+
+    public BigDecimal getIt_pkg_40HQ_qty() {
+        return it_pkg_40HQ_qty;
+    }
+
+    public void setIt_pkg_40HQ_qty(BigDecimal it_pkg_40HQ_qty) {
+        this.it_pkg_40HQ_qty = it_pkg_40HQ_qty;
+    }
+
+    public String getOff2_desc_offer_sh() {
+        return off2_desc_offer_sh;
+    }
+
+    public void setOff2_desc_offer_sh(String off2_desc_offer_sh) {
+        this.off2_desc_offer_sh = off2_desc_offer_sh;
+    }
+
+    public BigDecimal getCbm() {
+        return cbm;
+    }
+
+    public void setCbm(BigDecimal cbm) {
+        this.cbm = cbm;
+    }
+
+    public String getIt_uom() {
+        return it_uom;
+    }
+
+    public void setIt_uom(String it_uom) {
+        this.it_uom = it_uom;
+    }
+
+    public String getIt_pkg_exp_pcs() {
+        return it_pkg_exp_pcs;
+    }
+
+    public void setIt_pkg_exp_pcs(String it_pkg_exp_pcs) {
+        this.it_pkg_exp_pcs = it_pkg_exp_pcs;
+    }
+
+    public String getIt_image_1() {
+        return it_image_1;
+    }
+
+    public void setIt_image_1(String it_image_1) {
+        this.it_image_1 = it_image_1;
+    }
+
+    public String getIt_pkg_detail() {
+        return it_pkg_detail;
+    }
+
+    public void setIt_pkg_detail(String it_pkg_detail) {
+        this.it_pkg_detail = it_pkg_detail;
+    }
+
+    public String getIt_desc_of_sh() {
+        return it_desc_of_sh;
+    }
+
+    public void setIt_desc_of_sh(String it_desc_of_sh) {
+        this.it_desc_of_sh = it_desc_of_sh;
+    }
+
+}
