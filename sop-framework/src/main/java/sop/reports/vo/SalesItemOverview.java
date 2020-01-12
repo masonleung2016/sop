b6c7e8fcf0e1edc81e3dffa3cdfae00981068651
masonleung2016 <masonleung@163.com>
2020-01-12 12:17:53 +0800

    Create SalesItemOverview.java

---
+++ sop-framework/src/main/java/sop/reports/vo/SalesItemOverview.java
@@ -0,0 +1,203 @@
+package sop.reports.vo;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 10:25
+ * @Package: sop.reports.vo
+ */
+
+
+public class SalesItemOverview {
+    private String so_cu_code;
+    private String so_su_code;
+    private String so_no;
+    private String so_cu_po_no;
+    private String so_handle;
+    private String so2_ean;
+    private String itno;
+    private BigDecimal so2_uprice;
+    private BigDecimal no1;
+    private BigDecimal no2;
+    private BigDecimal no3;
+    private String cu_name;
+    private String su_name;
+    private String po2_no;
+    private BigDecimal po2_uprice;
+    private BigDecimal it_pkg_40HQ_qty;
+    private String it_image_1;
+    private String it_name;
+    private String year1;
+    private String year2;
+    private String year3;
+
+    public String getSo_cu_code() {
+        return so_cu_code;
+    }
+
+    public void setSo_cu_code(String so_cu_code) {
+        this.so_cu_code = so_cu_code;
+    }
+
+    public String getSo_su_code() {
+        return so_su_code;
+    }
+
+    public void setSo_su_code(String so_su_code) {
+        this.so_su_code = so_su_code;
+    }
+
+    public String getSo_no() {
+        return so_no;
+    }
+
+    public void setSo_no(String so_no) {
+        this.so_no = so_no;
+    }
+
+    public String getSo_cu_po_no() {
+        return so_cu_po_no;
+    }
+
+    public void setSo_cu_po_no(String so_cu_po_no) {
+        this.so_cu_po_no = so_cu_po_no;
+    }
+
+    public String getSo_handle() {
+        return so_handle;
+    }
+
+    public void setSo_handle(String so_handle) {
+        this.so_handle = so_handle;
+    }
+
+    public String getSo2_ean() {
+        return so2_ean;
+    }
+
+    public void setSo2_ean(String so2_ean) {
+        this.so2_ean = so2_ean;
+    }
+
+    public String getItno() {
+        return itno;
+    }
+
+    public void setItno(String itno) {
+        this.itno = itno;
+    }
+
+    public BigDecimal getSo2_uprice() {
+        return so2_uprice;
+    }
+
+    public void setSo2_uprice(BigDecimal so2_uprice) {
+        this.so2_uprice = so2_uprice;
+    }
+
+    public BigDecimal getNo1() {
+        return no1;
+    }
+
+    public void setNo1(BigDecimal no1) {
+        this.no1 = no1;
+    }
+
+    public BigDecimal getNo2() {
+        return no2;
+    }
+
+    public void setNo2(BigDecimal no2) {
+        this.no2 = no2;
+    }
+
+    public BigDecimal getNo3() {
+        return no3;
+    }
+
+    public void setNo3(BigDecimal no3) {
+        this.no3 = no3;
+    }
+
+    public String getCu_name() {
+        return cu_name;
+    }
+
+    public void setCu_name(String cu_name) {
+        this.cu_name = cu_name;
+    }
+
+    public String getSu_name() {
+        return su_name;
+    }
+
+    public void setSu_name(String su_name) {
+        this.su_name = su_name;
+    }
+
+    public String getPo2_no() {
+        return po2_no;
+    }
+
+    public void setPo2_no(String po2_no) {
+        this.po2_no = po2_no;
+    }
+
+    public BigDecimal getPo2_uprice() {
+        return po2_uprice;
+    }
+
+    public void setPo2_uprice(BigDecimal po2_uprice) {
+        this.po2_uprice = po2_uprice;
+    }
+
+    public BigDecimal getIt_pkg_40HQ_qty() {
+        return it_pkg_40HQ_qty;
+    }
+
+    public void setIt_pkg_40HQ_qty(BigDecimal it_pkg_40HQ_qty) {
+        this.it_pkg_40HQ_qty = it_pkg_40HQ_qty;
+    }
+
+    public String getIt_image_1() {
+        return it_image_1;
+    }
+
+    public void setIt_image_1(String it_image_1) {
+        this.it_image_1 = it_image_1;
+    }
+
+    public String getIt_name() {
+        return it_name;
+    }
+
+    public void setIt_name(String it_name) {
+        this.it_name = it_name;
+    }
+
+    public String getYear1() {
+        return year1;
+    }
+
+    public void setYear1(String year1) {
+        this.year1 = year1;
+    }
+
+    public String getYear2() {
+        return year2;
+    }
+
+    public void setYear2(String year2) {
+        this.year2 = year2;
+    }
+
+    public String getYear3() {
+        return year3;
+    }
+
+    public void setYear3(String year3) {
+        this.year3 = year3;
+    }
+
+}
