8b18df5ee79975a2132fc105f47425e6884b000e
masonleung2016 <masonleung@163.com>
2020-01-12 12:15:16 +0800

    Create OffListItem.java

---
+++ sop-framework/src/main/java/sop/reports/vo/OffListItem.java
@@ -0,0 +1,76 @@
+package sop.reports.vo;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 10:20
+ * @Package: sop.reports.vo
+ */
+
+
+public class OffListItem {
+    private String off_sh_no_pfix;
+    private String off_sh_no;
+    private String it_no;
+    private String off2_it_name;
+    private String off2_curr_code;
+    private String of_fob;
+    private String of_cnf;
+
+    public String getOff_sh_no_pfix() {
+        return off_sh_no_pfix;
+    }
+
+    public void setOff_sh_no_pfix(String off_sh_no_pfix) {
+        this.off_sh_no_pfix = off_sh_no_pfix;
+    }
+
+    public String getOff_sh_no() {
+        return off_sh_no;
+    }
+
+    public void setOff_sh_no(String off_sh_no) {
+        this.off_sh_no = off_sh_no;
+    }
+
+    public String getIt_no() {
+        return it_no;
+    }
+
+    public void setIt_no(String it_no) {
+        this.it_no = it_no;
+    }
+
+    public String getOff2_it_name() {
+        return off2_it_name;
+    }
+
+    public void setOff2_it_name(String off2_it_name) {
+        this.off2_it_name = off2_it_name;
+    }
+
+    public String getOff2_curr_code() {
+        return off2_curr_code;
+    }
+
+    public void setOff2_curr_code(String off2_curr_code) {
+        this.off2_curr_code = off2_curr_code;
+    }
+
+    public String getOf_fob() {
+        return of_fob;
+    }
+
+    public void setOf_fob(String of_fob) {
+        this.of_fob = of_fob;
+    }
+
+    public String getOf_cnf() {
+        return of_cnf;
+    }
+
+    public void setOf_cnf(String of_cnf) {
+        this.of_cnf = of_cnf;
+    }
+
+
+}
