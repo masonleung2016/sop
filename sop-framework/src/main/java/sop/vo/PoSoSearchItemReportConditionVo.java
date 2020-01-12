e40fe1a7efe3fb38d905b073257940e9471688b0
masonleung2016 <masonleung@163.com>
2020-01-12 12:43:23 +0800

    Create PoSoSearchItemReportConditionVo.java

---
+++ sop-framework/src/main/java/sop/vo/PoSoSearchItemReportConditionVo.java
@@ -0,0 +1,55 @@
+package sop.vo;
+
+import java.util.Date;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 11:46
+ * @Package: sop.vo
+ */
+
+
+public class PoSoSearchItemReportConditionVo extends SoPoConditionVo {
+    /**
+     * search list condition
+     */
+    private Date fromDate;
+
+    private Date toDate;
+
+    private String orderStatus;
+
+    private String handleUserId;
+
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(Date fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public Date getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(Date toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getHandleUserId() {
+        return handleUserId;
+    }
+
+    public void setHandleUserId(String handleUserId) {
+        this.handleUserId = handleUserId;
+    }
+}
