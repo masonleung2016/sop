18687f3f0126093f54149ce56ab038fc4de8b2f4
masonleung2016 <masonleung@163.com>
2020-01-12 12:40:21 +0800

    Create FactoryConditionVo.java

---
+++ sop-framework/src/main/java/sop/vo/FactoryConditionVo.java
@@ -0,0 +1,35 @@
+package sop.vo;
+
+import dwz.persistence.BaseConditionVO;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 11:37
+ * @Package: sop.vo
+ */
+
+public class FactoryConditionVo extends BaseConditionVO {
+    /**
+     * search list condition
+     */
+    private String fromNo;
+    private String toNo;
+
+    public String getFromNo() {
+        return fromNo;
+    }
+
+    public void setFromNo(String fromNo) {
+        this.fromNo = fromNo;
+    }
+
+    public String getToNo() {
+        return toNo;
+    }
+
+    public void setToNo(String toNo) {
+        this.toNo = toNo;
+    }
+
+
+}
