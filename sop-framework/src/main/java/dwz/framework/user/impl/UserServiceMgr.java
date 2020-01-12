798e7cb4337290cb25ab0e558be7bd0294361c38
masonleung2016 <masonleung@163.com>
2020-01-12 11:35:47 +0800

    Create UserServiceMgr.java

---
+++ sop-framework/src/main/java/dwz/framework/user/impl/UserServiceMgr.java
@@ -0,0 +1,16 @@
+package dwz.framework.user.impl;
+
+import java.util.List;
+
+import dwz.framework.user.User;
+import dwz.persistence.BaseConditionVO;
+import sop.persistence.beans.SysUser;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:14
+ * @Package: dwz.framework.user.impl
+ */
+
+public interface UserServiceMgr {
+}
