dd74e37fc26087e454b4ad631c536082ded3c2ce
masonleung2016 <masonleung@163.com>
2020-01-12 11:49:15 +0800

    Create DummyFileGenerator.java

---
+++ sop-framework/src/main/java/sop/filegen/generator/impl/DummyFileGenerator.java
@@ -0,0 +1,27 @@
+package sop.filegen.generator.impl;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+import sop.filegen.FileGeneratorConstant;
+import sop.filegen.GenRequest;
+import sop.filegen.GenResult;
+import sop.filegen.generator.FileGenerator;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:47
+ * @Package: sop.filegen.generator.impl
+ */
+
+public class DummyFileGenerator implements FileGenerator {
+
+    @Override
+    public GenResult generate(GenRequest request) {
+        GenResult result = new GenResult();
+        result.setReturnCode(FileGeneratorConstant.FAILED);
+        result.setMessage("System can not determine which report generator to be used. It may be due to empty reportType or outputType.");
+        ToStringBuilder.reflectionToString(request, ToStringStyle.MULTI_LINE_STYLE);
+        return result;
+    }
+}
