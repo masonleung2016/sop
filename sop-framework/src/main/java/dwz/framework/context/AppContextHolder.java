b1aa5c5d8add88dc069bbad785e20b08f13c1d3f
masonleung2016 <masonleung@163.com>
2020-01-12 11:20:57 +0800

    Create AppContextHolder.java

---
+++ sop-framework/src/main/java/dwz/framework/context/AppContextHolder.java
@@ -0,0 +1,27 @@
+package dwz.framework.context;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:37
+ * @Package: dwz.framework.context
+ */
+
+public class AppContextHolder {
+
+    private static final InheritableThreadLocal<AppContext> holder = new InheritableThreadLocal<AppContext>() {
+
+        protected AppContext initialValue() {
+            return null;
+        }
+
+    };
+
+    public static AppContext getContext() {
+        return holder.get();
+    }
+
+    public static void setContext(AppContext context) {
+        holder.set(context);
+    }
+
+}
