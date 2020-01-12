c9903c5e044f02766c4dc349a3c04ebf43035e9a
masonleung2016 <masonleung@163.com>
2020-01-12 11:27:18 +0800

    Create BusinessObject.java

---
+++ sop-framework/src/main/java/dwz/framework/sys/business/BusinessObject.java
@@ -0,0 +1,19 @@
+package dwz.framework.sys.business;
+
+import java.io.Serializable;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:57
+ * @Package: dwz.framework.sys.business
+ */
+
+public interface BusinessObject extends Serializable {
+
+    Serializable getId();
+
+    boolean isNew();
+
+    String getCacheKey();
+
+}
