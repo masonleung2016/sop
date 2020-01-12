563c6e4c429e2dc26ca81c9d0ee9789140476d37
masonleung2016 <masonleung@163.com>
2020-01-12 12:17:03 +0800

    Create PurchaseOrderReport.java

---
+++ sop-framework/src/main/java/sop/reports/vo/PurchaseOrderReport.java
@@ -0,0 +1,518 @@
+package sop.reports.vo;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 10:23
+ * @Package: sop.reports.vo
+ */
+
+
+public class PurchaseOrderReport {
+    private String pono;
+    private String podate;
+    private String suppname;
+    private String cntreq;
+    private String scno;
+    private String shipment;
+    private String etd;
+    private String dest;
+    private String payment;
+    private String fobport;
+    private String itno;
+    private String itname;
+    private String article;
+    private String ean;
+    private String appendix;
+    private String unit1;
+    private String unit2;
+    private BigDecimal qty;
+    private String uom;
+    private BigDecimal uprice;
+    private String curr;
+    private BigDecimal amt;
+    private String detail;
+    private String pkggenflag;
+    private String pkggenart;
+    private String pkggendtl;
+    private String pkggpflag;
+    private String pkggpart;
+    private String pkggpdtl;
+    private String pkgresyflag;
+    private String pkgresyart;
+    private String pkgresydtl;
+    private String pkgartnoflag;
+    private String pkgartnoart;
+    private String pkgartnodtl;
+    private String pkgeanflag;
+    private String pkgeanart;
+    private String pkgeandtl;
+    private String pkglangflag;
+    private String pkglangart;
+    private String pkglangdtl;
+    private String pkgotherflag;
+    private String pkgotherart;
+    private String pkgotherdtl;
+    private String pkginsflag;
+    private String pkginsart;
+    private String pkginsdtl;
+    private String pkgilablflag;
+    private String pkgilablart;
+    private String pkgilabldtl;
+    private String pkgiotherflag;
+    private String pkgiotherart;
+    private String pkgiotherdtl;
+    private String mainmark;
+    private String sidemark;
+    private String remark;
+
+    public String getPono() {
+        return pono;
+    }
+
+    public void setPono(String pono) {
+        this.pono = pono;
+    }
+
+    public String getPodate() {
+        return podate;
+    }
+
+    public void setPodate(String podate) {
+        this.podate = podate;
+    }
+
+    public String getSuppname() {
+        return suppname;
+    }
+
+    public void setSuppname(String suppname) {
+        this.suppname = suppname;
+    }
+
+    public String getCntreq() {
+        return cntreq;
+    }
+
+    public void setCntreq(String cntreq) {
+        this.cntreq = cntreq;
+    }
+
+    public String getScno() {
+        return scno;
+    }
+
+    public void setScno(String scno) {
+        this.scno = scno;
+    }
+
+    public String getShipment() {
+        return shipment;
+    }
+
+    public void setShipment(String shipment) {
+        this.shipment = shipment;
+    }
+
+    public String getEtd() {
+        return etd;
+    }
+
+    public void setEtd(String etd) {
+        this.etd = etd;
+    }
+
+    public String getDest() {
+        return dest;
+    }
+
+    public void setDest(String dest) {
+        this.dest = dest;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public String getFobport() {
+        return fobport;
+    }
+
+    public void setFobport(String fobport) {
+        this.fobport = fobport;
+    }
+
+    public String getItno() {
+        return itno;
+    }
+
+    public void setItno(String itno) {
+        this.itno = itno;
+    }
+
+    public String getItname() {
+        return itname;
+    }
+
+    public void setItname(String itname) {
+        this.itname = itname;
+    }
+
+    public String getArticle() {
+        return article;
+    }
+
+    public void setArticle(String article) {
+        this.article = article;
+    }
+
+    public String getEan() {
+        return ean;
+    }
+
+    public void setEan(String ean) {
+        this.ean = ean;
+    }
+
+    public String getAppendix() {
+        return appendix;
+    }
+
+    public void setAppendix(String appendix) {
+        this.appendix = appendix;
+    }
+
+    public String getUnit1() {
+        return unit1;
+    }
+
+    public void setUnit1(String unit1) {
+        this.unit1 = unit1;
+    }
+
+    public String getUnit2() {
+        return unit2;
+    }
+
+    public void setUnit2(String unit2) {
+        this.unit2 = unit2;
+    }
+
+    public BigDecimal getQty() {
+        return qty;
+    }
+
+    public void setQty(BigDecimal qty) {
+        this.qty = qty;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+
+    public BigDecimal getUprice() {
+        return uprice;
+    }
+
+    public void setUprice(BigDecimal uprice) {
+        this.uprice = uprice;
+    }
+
+    public String getCurr() {
+        return curr;
+    }
+
+    public void setCurr(String curr) {
+        this.curr = curr;
+    }
+
+    public BigDecimal getAmt() {
+        return amt;
+    }
+
+    public void setAmt(BigDecimal amt) {
+        this.amt = amt;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getPkggenflag() {
+        return pkggenflag;
+    }
+
+    public void setPkggenflag(String pkggenflag) {
+        this.pkggenflag = pkggenflag;
+    }
+
+    public String getPkggenart() {
+        return pkggenart;
+    }
+
+    public void setPkggenart(String pkggenart) {
+        this.pkggenart = pkggenart;
+    }
+
+    public String getPkggendtl() {
+        return pkggendtl;
+    }
+
+    public void setPkggendtl(String pkggendtl) {
+        this.pkggendtl = pkggendtl;
+    }
+
+    public String getPkggpflag() {
+        return pkggpflag;
+    }
+
+    public void setPkggpflag(String pkggpflag) {
+        this.pkggpflag = pkggpflag;
+    }
+
+    public String getPkggpart() {
+        return pkggpart;
+    }
+
+    public void setPkggpart(String pkggpart) {
+        this.pkggpart = pkggpart;
+    }
+
+    public String getPkggpdtl() {
+        return pkggpdtl;
+    }
+
+    public void setPkggpdtl(String pkggpdtl) {
+        this.pkggpdtl = pkggpdtl;
+    }
+
+    public String getPkgresyflag() {
+        return pkgresyflag;
+    }
+
+    public void setPkgresyflag(String pkgresyflag) {
+        this.pkgresyflag = pkgresyflag;
+    }
+
+    public String getPkgresyart() {
+        return pkgresyart;
+    }
+
+    public void setPkgresyart(String pkgresyart) {
+        this.pkgresyart = pkgresyart;
+    }
+
+    public String getPkgresydtl() {
+        return pkgresydtl;
+    }
+
+    public void setPkgresydtl(String pkgresydtl) {
+        this.pkgresydtl = pkgresydtl;
+    }
+
+    public String getPkgartnoflag() {
+        return pkgartnoflag;
+    }
+
+    public void setPkgartnoflag(String pkgartnoflag) {
+        this.pkgartnoflag = pkgartnoflag;
+    }
+
+    public String getPkgartnoart() {
+        return pkgartnoart;
+    }
+
+    public void setPkgartnoart(String pkgartnoart) {
+        this.pkgartnoart = pkgartnoart;
+    }
+
+    public String getPkgartnodtl() {
+        return pkgartnodtl;
+    }
+
+    public void setPkgartnodtl(String pkgartnodtl) {
+        this.pkgartnodtl = pkgartnodtl;
+    }
+
+    public String getPkgeanflag() {
+        return pkgeanflag;
+    }
+
+    public void setPkgeanflag(String pkgeanflag) {
+        this.pkgeanflag = pkgeanflag;
+    }
+
+    public String getPkgeanart() {
+        return pkgeanart;
+    }
+
+    public void setPkgeanart(String pkgeanart) {
+        this.pkgeanart = pkgeanart;
+    }
+
+    public String getPkgeandtl() {
+        return pkgeandtl;
+    }
+
+    public void setPkgeandtl(String pkgeandtl) {
+        this.pkgeandtl = pkgeandtl;
+    }
+
+    public String getPkglangflag() {
+        return pkglangflag;
+    }
+
+    public void setPkglangflag(String pkglangflag) {
+        this.pkglangflag = pkglangflag;
+    }
+
+    public String getPkglangart() {
+        return pkglangart;
+    }
+
+    public void setPkglangart(String pkglangart) {
+        this.pkglangart = pkglangart;
+    }
+
+    public String getPkglangdtl() {
+        return pkglangdtl;
+    }
+
+    public void setPkglangdtl(String pkglangdtl) {
+        this.pkglangdtl = pkglangdtl;
+    }
+
+    public String getPkgotherflag() {
+        return pkgotherflag;
+    }
+
+    public void setPkgotherflag(String pkgotherflag) {
+        this.pkgotherflag = pkgotherflag;
+    }
+
+    public String getPkgotherart() {
+        return pkgotherart;
+    }
+
+    public void setPkgotherart(String pkgotherart) {
+        this.pkgotherart = pkgotherart;
+    }
+
+    public String getPkgotherdtl() {
+        return pkgotherdtl;
+    }
+
+    public void setPkgotherdtl(String pkgotherdtl) {
+        this.pkgotherdtl = pkgotherdtl;
+    }
+
+    public String getPkginsflag() {
+        return pkginsflag;
+    }
+
+    public void setPkginsflag(String pkginsflag) {
+        this.pkginsflag = pkginsflag;
+    }
+
+    public String getPkginsart() {
+        return pkginsart;
+    }
+
+    public void setPkginsart(String pkginsart) {
+        this.pkginsart = pkginsart;
+    }
+
+    public String getPkginsdtl() {
+        return pkginsdtl;
+    }
+
+    public void setPkginsdtl(String pkginsdtl) {
+        this.pkginsdtl = pkginsdtl;
+    }
+
+    public String getPkgilablflag() {
+        return pkgilablflag;
+    }
+
+    public void setPkgilablflag(String pkgilablflag) {
+        this.pkgilablflag = pkgilablflag;
+    }
+
+    public String getPkgilablart() {
+        return pkgilablart;
+    }
+
+    public void setPkgilablart(String pkgilablart) {
+        this.pkgilablart = pkgilablart;
+    }
+
+    public String getPkgilabldtl() {
+        return pkgilabldtl;
+    }
+
+    public void setPkgilabldtl(String pkgilabldtl) {
+        this.pkgilabldtl = pkgilabldtl;
+    }
+
+    public String getPkgiotherflag() {
+        return pkgiotherflag;
+    }
+
+    public void setPkgiotherflag(String pkgiotherflag) {
+        this.pkgiotherflag = pkgiotherflag;
+    }
+
+    public String getPkgiotherart() {
+        return pkgiotherart;
+    }
+
+    public void setPkgiotherart(String pkgiotherart) {
+        this.pkgiotherart = pkgiotherart;
+    }
+
+    public String getPkgiotherdtl() {
+        return pkgiotherdtl;
+    }
+
+    public void setPkgiotherdtl(String pkgiotherdtl) {
+        this.pkgiotherdtl = pkgiotherdtl;
+    }
+
+    public String getMainmark() {
+        return mainmark;
+    }
+
+    public void setMainmark(String mainmark) {
+        this.mainmark = mainmark;
+    }
+
+    public String getSidemark() {
+        return sidemark;
+    }
+
+    public void setSidemark(String sidemark) {
+        this.sidemark = sidemark;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+}
