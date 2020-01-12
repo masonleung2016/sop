b83ea723548b046b6335cfd1882923698b42cb2c
masonleung2016 <masonleung@163.com>
2020-01-12 12:11:43 +0800

    Create Staff.java

---
+++ sop-framework/src/main/java/sop/persistence/beans/Staff.java
@@ -0,0 +1,42 @@
+package sop.persistence.beans;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 10:08
+ * @Package: sop.persistence.beans
+ */
+
+public class Staff extends BaseBean {
+    /**
+     * table staff
+     */
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String sfCode;
+    private String sfName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSfCode() {
+        return sfCode;
+    }
+
+    public void setSfCode(String sfCode) {
+        this.sfCode = sfCode;
+    }
+
+    public String getSfName() {
+        return sfName;
+    }
+
+    public void setSfName(String sfName) {
+        this.sfName = sfName;
+    }
+}
