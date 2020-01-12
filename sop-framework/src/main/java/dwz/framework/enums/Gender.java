33098a04234137f67f41b0dfd8eceff6c35b86e6
masonleung2016 <masonleung@163.com>
2020-01-12 11:22:02 +0800

    Create Gender.java

---
+++ sop-framework/src/main/java/dwz/framework/enums/Gender.java
@@ -0,0 +1,26 @@
+package dwz.framework.enums;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:39
+ * @Package: dwz.framework.enums
+ */
+
+public enum Gender {
+    male("男"), female("女");
+
+    private String name;
+
+    private Gender(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
