dedd15b388b91592cfdfadb9b0a56c3d61c8d582
masonleung2016 <masonleung@163.com>
2020-01-13 00:23:10 +0800

    Create SoPoConditionVo.java

---
+++ sop-framework/src/main/java/sop/vo/SoPoConditionVo.java
@@ -0,0 +1,30 @@
+package sop.vo;
+
+import dwz.persistence.BaseConditionVO;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 12:38
+ * @Package: sop.vo
+ */
+
+public class SoPoConditionVo extends BaseConditionVO {
+    private String fromNo;
+    private String toNo;
+
+    public String getFromNo() {
+        return fromNo;
+    }
+
+    public void setFromNo(String fromNo) {
+        this.fromNo = fromNo;
+    }
+
+    public String getToNo() {
+        return toNo;
+    }
+
+    public void setToNo(String toNo) {
+        this.toNo = toNo;
+    }
+}
