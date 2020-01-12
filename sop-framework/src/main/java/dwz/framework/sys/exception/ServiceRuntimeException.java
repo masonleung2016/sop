36699d46c25b3e974f48a99947417ebc5d585a86
masonleung2016 <masonleung@163.com>
2020-01-12 11:28:40 +0800

    Create ServiceRuntimeException.java

---
+++ sop-framework/src/main/java/dwz/framework/sys/exception/ServiceRuntimeException.java
@@ -0,0 +1,35 @@
+package dwz.framework.sys.exception;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:00
+ * @Package: dwz.framework.sys.exception
+ */
+public class ServiceRuntimeException extends RuntimeException {
+
+    private static final long serialVersionUID = -6879298762342455L;
+
+    public ServiceRuntimeException() {
+        super();
+    }
+
+    public ServiceRuntimeException(String message) {
+        super(message);
+    }
+
+    public ServiceRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    public ServiceRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * 出于性能考虑， 复写此方法。
+     */
+    public Throwable fillInStackTrace() {
+        return this;
+    }
+
+}
