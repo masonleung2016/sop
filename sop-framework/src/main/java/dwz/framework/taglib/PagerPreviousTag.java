cbfcf31284d14058bea01f108d2d606a7ac81236
masonleung2016 <masonleung@163.com>
2020-01-12 11:31:37 +0800

    Create PagerPreviousTag.java

---
+++ sop-framework/src/main/java/dwz/framework/taglib/PagerPreviousTag.java
@@ -0,0 +1,48 @@
+package dwz.framework.taglib;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:07
+ * @Package: dwz.framework.taglib
+ */
+
+public class PagerPreviousTag extends TagSupport {
+
+    private static final long serialVersionUID = -8087949809819566869L;
+
+    private int prePage;
+
+    private int currentPageNum;
+
+    public int doStartTag() throws JspException {
+        PagerInfoTag tag = (PagerInfoTag) findAncestorWithClass(this, PagerInfoTag.class);
+
+        if (tag == null) {
+            throw new JspTagException("The root tag not found!");
+        }
+        try {
+            currentPageNum = Integer.parseInt(tag.getCurrentPageNum());
+        } catch (Exception e) {
+            throw new JspException(e);
+        }
+        if (currentPageNum > 1)
+            prePage = currentPageNum - 1;
+        else
+            prePage = 1;
+        if (currentPageNum <= 1) {
+            return SKIP_BODY;
+        }
+        pageContext.setAttribute("prePage", prePage);
+        return EVAL_BODY_INCLUDE;
+    }
+
+    public int doEndTag() throws JspException {
+        pageContext.removeAttribute("prePage");
+        return EVAL_PAGE;
+    }
+
+}
