682e39c24c55de1b48eecf263fe72b6995b8a39e
masonleung2016 <masonleung@163.com>
2020-01-12 11:41:20 +0800

    Create ProductcatMapper.java

---
+++ sop-framework/src/main/java/dwz/persistence/mapper/ProductcatMapper.java
@@ -0,0 +1,54 @@
+package dwz.persistence.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.stereotype.Repository;
+
+import dwz.dal.BaseMapper;
+import dwz.persistence.BaseConditionVO;
+import sop.persistence.beans.Company;
+import sop.persistence.beans.ProductCategory;
+import sop.vo.CompanyVo;
+import sop.vo.ProductCategoryVo;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:28
+ * @Package: dwz.persistence.mapper
+ */
+
+
+@Repository
+public interface ProductcatMapper extends BaseMapper<Company, Integer> {
+
+    List<CompanyVo> getComListByCondition(BaseConditionVO vo, RowBounds rb);
+
+    Integer getComListNumByCondition(BaseConditionVO vo);
+
+    Company getCompanyByFkCoCode(String fkCoCode);
+
+    void updateCompany(Company currentCom);
+
+    Integer getCountByFkCoCode(String fkCoCode);
+
+    Integer insertCompany(Company currentCom);
+
+    List<ProductCategoryVo> getProductcatListByCondition(BaseConditionVO vo,
+                                                         RowBounds rb);
+
+    Integer getProductcatListNumByCondition(BaseConditionVO vo);
+
+    ProductCategory getProductcatByFkCatCode(String fkCatCode);
+
+    void updateProductcat(ProductCategory currentProductcat);
+
+    Integer getCountByFkCatCode(String fkCatCode);
+
+    Integer insertProductcat(ProductCategory currentProductcat);
+
+    Integer deleteProductcat(String fkCatCode);
+
+    List<ProductCategoryVo> getAllProductCats();
+
+}
