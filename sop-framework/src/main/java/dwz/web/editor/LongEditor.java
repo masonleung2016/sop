a69b01e540412e5a2a4cc64930d280441e773064
masonleung2016 <masonleung@163.com>
2020-01-12 11:45:18 +0800

    Create LongEditor.java

---
+++ sop-framework/src/main/java/dwz/web/editor/LongEditor.java
@@ -0,0 +1,31 @@
+package dwz.web.editor;
+
+import java.beans.PropertyEditorSupport;
+
+import org.springframework.util.StringUtils;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:38
+ * @Package: dwz.web.editor
+ */
+
+public class LongEditor extends PropertyEditorSupport {
+    @Override
+    public String getAsText() {
+
+        return getValue().toString();
+    }
+
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        if (text == null || text.equals(""))
+            text = "0";
+        if (!StringUtils.hasText(text)) {
+            setValue(null);
+        } else {
+            setValue(Long.parseLong(text));
+            // 这句话是最重要的，他的目的是通过传入参数的类型来匹配相应的databind
+        }
+    }
+}
