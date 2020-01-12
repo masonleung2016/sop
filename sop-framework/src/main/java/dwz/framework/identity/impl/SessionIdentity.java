2911c536bc884d3291720da639e2b02542cfad64
masonleung2016 <masonleung@163.com>
2020-01-12 11:22:53 +0800

    Create SessionIdentity.java

---
+++ sop-framework/src/main/java/dwz/framework/identity/impl/SessionIdentity.java
@@ -0,0 +1,29 @@
+package dwz.framework.identity.impl;
+
+import java.io.Serializable;
+
+import dwz.framework.identity.Identity;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:40
+ * @Package: dwz.framework.identity.impl
+ */
+
+public class SessionIdentity implements Identity {
+
+    private Serializable accessToken = null;
+
+    public SessionIdentity(Serializable accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Serializable getAccessToken() {
+        return this.accessToken;
+    }
+
+    public void setAccessToken(Serializable accessToken) {
+        this.accessToken = accessToken;
+    }
+
+}
