7435b16bb707674227a37602e5c3c604a75799d0
masonleung2016 <masonleung@163.com>
2020-01-12 12:30:10 +0800

    Create PayTermServiceMgr.java

---
+++ sop-framework/src/main/java/sop/services/PayTermServiceMgr.java
@@ -0,0 +1,39 @@
+package sop.services;
+
+import java.util.List;
+
+import dwz.framework.sys.business.BusinessObjectServiceMgr;
+import dwz.framework.sys.exception.ServiceException;
+import dwz.framework.vo.Checker;
+import dwz.persistence.BaseConditionVO;
+import sop.persistence.beans.PayTerm;
+import sop.vo.PayTermVo;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 11:07
+ * @Package: sop.services
+ */
+
+
+public interface PayTermServiceMgr extends BusinessObjectServiceMgr {
+    String SERVICE_NAME = "payTermServiceMgr";
+
+    List<PayTermVo> searchPayTerm(BaseConditionVO vo);
+
+    Integer searchPayTermNum(BaseConditionVO vo);
+
+    boolean addPayTerm(PayTerm payTerm) throws ServiceException;
+
+    PayTerm getPayTerm(String code);
+
+    boolean updatePayTerm(PayTerm payTerm) throws ServiceException;
+
+    Checker checkPaytm(PayTerm payTerm);
+
+    boolean deletePaymentTerm(String payCode) throws ServiceException;
+
+    Checker checkDecimal(PayTerm payTerm);
+
+
+}
