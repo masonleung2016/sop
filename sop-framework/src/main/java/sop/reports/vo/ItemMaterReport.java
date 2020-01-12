970c2d3b484c6bb286929acd1de8658f577e7401
masonleung2016 <masonleung@163.com>
2020-01-12 12:14:47 +0800

    Create ItemMaterReport.java

---
+++ sop-framework/src/main/java/sop/reports/vo/ItemMaterReport.java
@@ -0,0 +1,178 @@
+package sop.reports.vo;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 10:12
+ * @Package: sop.reports.vo
+ */
+
+
+public class ItemMaterReport {
+
+    private String it_cat;
+    private String it_no;
+    private String it_suffix;
+    private String it_name;
+    private BigDecimal it_pkg_20_qty;
+    private BigDecimal it_pkg_40_qty;
+    private BigDecimal it_cbm_total;
+    private BigDecimal it_pkg_l_cm;
+    private BigDecimal it_pkg_w_cm;
+    private BigDecimal it_pkg_h_cm;
+    private String it_uom_inn;
+    private String it_pkg_exp_pcs;
+    private String it_pkg_detail;
+    private String it_desc_of_sh;
+    private String it_desc_fty_po;
+    private String it_fty_name;
+    private String it_image_1;
+    private String it_fty_code;
+
+    public String getIt_fty_code() {
+        return it_fty_code;
+    }
+
+    public void setIt_fty_code(String it_fty_code) {
+        this.it_fty_code = it_fty_code;
+    }
+
+    public String getIt_image_1() {
+        return it_image_1;
+    }
+
+    public void setIt_image_1(String it_image_1) {
+        this.it_image_1 = it_image_1;
+    }
+
+    public String getIt_cat() {
+        return it_cat;
+    }
+
+    public void setIt_cat(String it_cat) {
+        this.it_cat = it_cat;
+    }
+
+    public String getIt_no() {
+        return it_no;
+    }
+
+    public void setIt_no(String it_no) {
+        this.it_no = it_no;
+    }
+
+    public String getIt_suffix() {
+        return it_suffix;
+    }
+
+    public void setIt_suffix(String it_suffix) {
+        this.it_suffix = it_suffix;
+    }
+
+    public String getIt_name() {
+        return it_name;
+    }
+
+    public void setIt_name(String it_name) {
+        this.it_name = it_name;
+    }
+
+    public BigDecimal getIt_pkg_20_qty() {
+        return it_pkg_20_qty;
+    }
+
+    public void setIt_pkg_20_qty(BigDecimal it_pkg_20_qty) {
+        this.it_pkg_20_qty = it_pkg_20_qty;
+    }
+
+    public BigDecimal getIt_pkg_40_qty() {
+        return it_pkg_40_qty;
+    }
+
+    public void setIt_pkg_40_qty(BigDecimal it_pkg_40_qty) {
+        this.it_pkg_40_qty = it_pkg_40_qty;
+    }
+
+    public BigDecimal getIt_cbm_total() {
+        return it_cbm_total;
+    }
+
+    public void setIt_cbm_total(BigDecimal it_cbm_total) {
+        this.it_cbm_total = it_cbm_total;
+    }
+
+    public BigDecimal getIt_pkg_l_cm() {
+        return it_pkg_l_cm;
+    }
+
+    public void setIt_pkg_l_cm(BigDecimal it_pkg_l_cm) {
+        this.it_pkg_l_cm = it_pkg_l_cm;
+    }
+
+    public BigDecimal getIt_pkg_w_cm() {
+        return it_pkg_w_cm;
+    }
+
+    public void setIt_pkg_w_cm(BigDecimal it_pkg_w_cm) {
+        this.it_pkg_w_cm = it_pkg_w_cm;
+    }
+
+    public BigDecimal getIt_pkg_h_cm() {
+        return it_pkg_h_cm;
+    }
+
+    public void setIt_pkg_h_cm(BigDecimal it_pkg_h_cm) {
+        this.it_pkg_h_cm = it_pkg_h_cm;
+    }
+
+    public String getIt_uom_inn() {
+        return it_uom_inn;
+    }
+
+    public void setIt_uom_inn(String it_uom_inn) {
+        this.it_uom_inn = it_uom_inn;
+    }
+
+    public String getIt_pkg_exp_pcs() {
+        return it_pkg_exp_pcs;
+    }
+
+    public void setIt_pkg_exp_pcs(String it_pkg_exp_pcs) {
+        this.it_pkg_exp_pcs = it_pkg_exp_pcs;
+    }
+
+    public String getIt_pkg_detail() {
+        return it_pkg_detail;
+    }
+
+    public void setIt_pkg_detail(String it_pkg_detail) {
+        this.it_pkg_detail = it_pkg_detail;
+    }
+
+    public String getIt_desc_of_sh() {
+        return it_desc_of_sh;
+    }
+
+    public void setIt_desc_of_sh(String it_desc_of_sh) {
+        this.it_desc_of_sh = it_desc_of_sh;
+    }
+
+    public String getIt_desc_fty_po() {
+        return it_desc_fty_po;
+    }
+
+    public void setIt_desc_fty_po(String it_desc_fty_po) {
+        this.it_desc_fty_po = it_desc_fty_po;
+    }
+
+    public String getIt_fty_name() {
+        return it_fty_name;
+    }
+
+    public void setIt_fty_name(String it_fty_name) {
+        this.it_fty_name = it_fty_name;
+    }
+
+
+}
