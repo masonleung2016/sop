0ef45fc7d867b83967eeb5e7fe0407841d637910
masonleung2016 <masonleung@163.com>
2020-01-12 12:30:41 +0800

    Create ProductItemServiceMgr.java

---
+++ sop-framework/src/main/java/sop/services/ProductItemServiceMgr.java
@@ -0,0 +1,20 @@
+package sop.services;
+
+import java.util.List;
+
+import dwz.framework.sys.business.BusinessObjectServiceMgr;
+import dwz.persistence.BaseConditionVO;
+import sop.persistence.beans.ProductItem;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 11:10
+ * @Package: sop.services
+ */
+
+public interface ProductItemServiceMgr extends BusinessObjectServiceMgr {
+    String SERVICE_NAME = "productItemServiceMgr";
+
+    List<ProductItem> searchProductItem(BaseConditionVO vo);
+
+}
