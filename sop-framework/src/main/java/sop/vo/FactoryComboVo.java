afa9fd86c059d51a6f204074563e111db74ee1f7
masonleung2016 <masonleung@163.com>
2020-01-12 12:40:08 +0800

    Create FactoryComboVo.java

---
+++ sop-framework/src/main/java/sop/vo/FactoryComboVo.java
@@ -0,0 +1,21 @@
+package sop.vo;
+
+import java.util.List;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 11:37
+ * @Package: sop.vo
+ */
+
+public class FactoryComboVo {
+    private List<PayTermVo> payTerms;
+
+    public List<PayTermVo> getPayTerms() {
+        return payTerms;
+    }
+
+    public void setPayTerms(List<PayTermVo> payTerms) {
+        this.payTerms = payTerms;
+    }
+}
