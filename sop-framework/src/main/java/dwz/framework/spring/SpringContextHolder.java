76e9c15694103929a3cf67d145a424b09ae41bc7
masonleung2016 <masonleung@163.com>
2020-01-12 11:26:01 +0800

    Create SpringContextHolder.java

---
+++ sop-framework/src/main/java/dwz/framework/spring/SpringContextHolder.java
@@ -0,0 +1,58 @@
+package dwz.framework.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:53
+ * @Package: dwz.framework.spring
+ */
+public class SpringContextHolder implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    /**
+     * 得到Spring 上下文环境
+     *
+     * @return
+     */
+    public static ApplicationContext getApplicationContext() {
+        checkApplicationContext();
+        return applicationContext;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        SpringContextHolder.applicationContext = applicationContext;
+    }
+
+    /**
+     * 通过Spring Bean name 得到Bean
+     *
+     * @param name bean 上下文定义名称
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(String name) {
+        checkApplicationContext();
+        return (T) applicationContext.getBean(name);
+    }
+
+    /**
+     * 通过类型得到Bean
+     *
+     * @param clazz
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(Class<T> clazz) {
+        checkApplicationContext();
+        return (T) applicationContext.getBeansOfType(clazz);
+    }
+
+    private static void checkApplicationContext() {
+        if (applicationContext == null) {
+            throw new IllegalStateException("applicaitonContext未注入,请在application-context.xml中定义SpringContextHolder");
+        }
+    }
+
+}
