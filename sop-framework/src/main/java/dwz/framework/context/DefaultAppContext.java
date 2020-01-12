656a00be2fdd9815670b1b35de25af92562f2926
masonleung2016 <masonleung@163.com>
2020-01-12 11:21:08 +0800

    Create DefaultAppContext.java

---
+++ sop-framework/src/main/java/dwz/framework/context/DefaultAppContext.java
@@ -0,0 +1,27 @@
+package dwz.framework.context;
+
+import dwz.framework.user.User;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:38
+ * @Package: dwz.framework.context
+ */
+
+public class DefaultAppContext implements AppContext {
+
+    private User user = null;
+
+    public DefaultAppContext() {
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+}
