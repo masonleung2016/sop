8024c5f145957e283e1b51a3967937a86c575c95
masonleung2016 <masonleung@163.com>
2020-01-12 11:53:24 +0800

    Create MailMergeEnginelService.java

---
+++ sop-framework/src/main/java/sop/filegen/generator/impl/MailMergeEnginelService.java
@@ -0,0 +1,22 @@
+package sop.filegen.generator.impl;
+
+import fr.opensagres.xdocreport.template.formatter.FieldsMetadata;
+import sop.filegen.GenRequest;
+import sop.filegen.GenResult;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:57
+ * @Package: sop.filegen.generator.impl
+ */
+
+public interface MailMergeEnginelService {
+
+    String toMailMerge(String screenID, String templateID, GenRequest request, GenResult response);
+
+    String toMailMerge(String screenID, String templateID, GenRequest request, GenResult response, Object object, FieldsMetadata metadata);
+
+    void initCommonInfoForGenDOC(Integer caseKey, String screenID,
+                                 String templateID, GenRequest request);
+
+}
