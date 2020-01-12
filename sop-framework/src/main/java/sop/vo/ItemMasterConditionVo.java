9d1c254ae29d182886c9c54704a61f084f7690cc
masonleung2016 <masonleung@163.com>
2020-01-12 12:41:05 +0800

    Create ItemMasterConditionVo.java

---
+++ sop-framework/src/main/java/sop/vo/ItemMasterConditionVo.java
@@ -0,0 +1,35 @@
+package sop.vo;
+
+import java.io.Serializable;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 11:40
+ * @Package: sop.vo
+ */
+
+public class ItemMasterConditionVo implements Serializable {
+
+    private static final long serialVersionUID = 6237430476528427602L;
+
+    private String fromNo;
+    private String toNo;
+
+    public String getFromNo() {
+        return fromNo;
+    }
+
+    public void setFromNo(String fromNo) {
+        this.fromNo = fromNo;
+    }
+
+    public String getToNo() {
+        return toNo;
+    }
+
+    public void setToNo(String toNo) {
+        this.toNo = toNo;
+    }
+
+
+}
