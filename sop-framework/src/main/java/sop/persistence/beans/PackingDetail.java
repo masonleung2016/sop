787e9b132d0667d18d3c52ad33a74f0976b4b59f
masonleung2016 <masonleung@163.com>
2020-01-12 12:06:29 +0800

    Create PackingDetail.java

---
+++ sop-framework/src/main/java/sop/persistence/beans/PackingDetail.java
@@ -0,0 +1,54 @@
+package sop.persistence.beans;
+
+import java.io.Serializable;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 9:54
+ * @Package: sop.persistence.beans
+ */
+
+public class PackingDetail extends BaseBean {
+    private String pdNo;
+    private String category;
+    private String template;
+    private String pdDetail;
+
+    public String getPdNo() {
+        return pdNo;
+    }
+
+    public void setPdNo(String pdNo) {
+        this.pdNo = pdNo;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    public String getPdDetail() {
+        return pdDetail;
+    }
+
+    public void setPdDetail(String pdDetail) {
+        this.pdDetail = pdDetail;
+    }
+
+    @Override
+    public Serializable getId() {
+        return this.pdNo;
+    }
+
+}
