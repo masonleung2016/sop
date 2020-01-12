89c1b1268391dd6540b0e490a39ee68819901a2b
masonleung2016 <masonleung@163.com>
2020-01-12 12:37:09 +0800

    Create BarcodeUtil.java

---
+++ sop-framework/src/main/java/sop/utils/BarcodeUtil.java
@@ -0,0 +1,102 @@
+package sop.utils;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.apache.commons.lang.StringUtils;
+import org.krysalis.barcode4j.ChecksumMode;
+import org.krysalis.barcode4j.impl.code128.EAN128Bean;
+import org.krysalis.barcode4j.impl.code39.Code39Bean;
+import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider;
+import org.krysalis.barcode4j.tools.UnitConv;
+
+/**
+ * 条形码工具类
+ *
+ * @Author: LCF
+ * @Date: 2020/1/9 11:25
+ * @Package: sop.utils
+ */
+
+public class BarcodeUtil {
+
+    /**
+     * 生成文件
+     *
+     * @param msg
+     * @param path
+     * @return
+     */
+    public static File generateFile(String msg, String path) {
+        File file = new File(path);
+        try {
+            generate(msg, new FileOutputStream(file));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return file;
+    }
+
+    /**
+     * 生成字节
+     *
+     * @param msg
+     * @return
+     */
+    public static byte[] generate(String msg) {
+        ByteArrayOutputStream ous = new ByteArrayOutputStream();
+        generate(msg, ous);
+        return ous.toByteArray();
+    }
+
+    /**
+     * 生成到流
+     *
+     * @param msg
+     * @param ous
+     */
+    public static void generate(String msg, OutputStream ous) {
+        if (StringUtils.isEmpty(msg) || ous == null) {
+            return;
+        }
+
+        EAN128Bean bean = new EAN128Bean();
+
+        // 精细度
+        final int dpi = 100;
+        // module宽度
+        final double moduleWidth = UnitConv.in2mm(1.8f / dpi);
+
+        // 配置对象
+        bean.setModuleWidth(moduleWidth);
+        //bean.setWideFactor(3);
+        //bean.doQuietZone(false);
+
+        String format = "image/png";
+        try {
+
+            // 输出到流
+            BitmapCanvasProvider canvas = new BitmapCanvasProvider(ous, format, dpi,
+                    BufferedImage.TYPE_BYTE_GRAY, false, 0);
+
+            // 生成条形码
+            bean.generateBarcode(canvas, msg);
+
+            // 结束绘制
+            canvas.finish();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        String msg = "788515004012";
+        String path = "barcode.png";
+        generateFile(msg, path);
+    }
+}
