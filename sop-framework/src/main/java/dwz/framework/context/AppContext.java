ceb7100bba47c04e23950df9f58b3e3c09043bf6
masonleung2016 <masonleung@163.com>
2020-01-12 11:20:39 +0800

    Create AppContext.java

---
+++ sop-framework/src/main/java/dwz/framework/context/AppContext.java
@@ -0,0 +1,17 @@
+package dwz.framework.context;
+
+import dwz.framework.user.User;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:37
+ * @Package: dwz.framework.context
+ */
+public interface AppContext {
+
+    User getUser();
+
+    void setUser(User user);
+
+
+}
