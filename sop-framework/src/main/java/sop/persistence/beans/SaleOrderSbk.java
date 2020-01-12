0e672982470e2e72c0bbede2bba05fda9cdeaf47
masonleung2016 <masonleung@163.com>
2020-01-12 12:11:27 +0800

    Create SaleOrderSbk.java

---
+++ sop-framework/src/main/java/sop/persistence/beans/SaleOrderSbk.java
@@ -0,0 +1,92 @@
+package sop.persistence.beans;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 10:07
+ * @Package: sop.persistence.beans
+ */
+
+
+public class SaleOrderSbk extends BaseBean {
+
+    /**
+     * sotx3sbk
+     */
+    private static final long serialVersionUID = -185248557316796065L;
+    private Integer id;
+    /**
+     * co_code
+     */
+    private String coCode;
+    /**
+     * so3_no
+     */
+    private String so3No;
+    /**
+     * so3_exp_shp_date
+     */
+    private Date so3ExpShpDate;
+    /**
+     * so3_exp_shp_qty
+     */
+    private BigDecimal so3ExpShpQty;
+    /**
+     * so3_remk
+     */
+    private String so3Remk;
+    public SaleOrderSbk() {
+
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCoCode() {
+        return coCode;
+    }
+
+    public void setCoCode(String coCode) {
+        this.coCode = coCode;
+    }
+
+    public String getSo3No() {
+        return so3No;
+    }
+
+    public void setSo3No(String so3No) {
+        this.so3No = so3No;
+    }
+
+    public Date getSo3ExpShpDate() {
+        return so3ExpShpDate;
+    }
+
+    public void setSo3ExpShpDate(Date so3ExpShpDate) {
+        this.so3ExpShpDate = so3ExpShpDate;
+    }
+
+    public BigDecimal getSo3ExpShpQty() {
+        return so3ExpShpQty;
+    }
+
+    public void setSo3ExpShpQty(BigDecimal so3ExpShpQty) {
+        this.so3ExpShpQty = so3ExpShpQty;
+    }
+
+    public String getSo3Remk() {
+        return so3Remk;
+    }
+
+    public void setSo3Remk(String so3Remk) {
+        this.so3Remk = so3Remk;
+    }
+}
