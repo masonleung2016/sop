fecef6af024c7b9b477ff49738ed0bf05157bb37
masonleung2016 <masonleung@163.com>
2020-01-12 12:19:36 +0800

    Create SoldItemSummaryBySoWPhoto.java

---
+++ sop-framework/src/main/java/sop/reports/vo/SoldItemSummaryBySoWPhoto.java
@@ -0,0 +1,37 @@
+package sop.reports.vo;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/9 10:27
+ * @Package: sop.reports.vo
+ */
+
+public class SoldItemSummaryBySoWPhoto {
+    private String it_cat_no_suffix;
+    private String so2_cu_it_no;
+    private String it_image_1;
+
+    public String getIt_cat_no_suffix() {
+        return it_cat_no_suffix;
+    }
+
+    public void setIt_cat_no_suffix(String it_cat_no_suffix) {
+        this.it_cat_no_suffix = it_cat_no_suffix;
+    }
+
+    public String getSo2_cu_it_no() {
+        return so2_cu_it_no;
+    }
+
+    public void setSo2_cu_it_no(String so2_cu_it_no) {
+        this.so2_cu_it_no = so2_cu_it_no;
+    }
+
+    public String getIt_image_1() {
+        return it_image_1;
+    }
+
+    public void setIt_image_1(String it_image_1) {
+        this.it_image_1 = it_image_1;
+    }
+}
