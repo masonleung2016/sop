464f194b65f8cd149b8e76125b2878cba54d6ce4
masonleung2016 <masonleung@163.com>
2020-01-12 11:42:30 +0800

    Create SupplierMapper.java

---
+++ sop-framework/src/main/java/dwz/persistence/mapper/SupplierMapper.java
@@ -0,0 +1,32 @@
+package dwz.persistence.mapper;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.stereotype.Repository;
+
+import dwz.dal.BaseMapper;
+import dwz.persistence.BaseConditionVO;
+import sop.persistence.beans.Supplier;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:32
+ * @Package: dwz.persistence.mapper
+ */
+
+@Repository
+public interface SupplierMapper extends BaseMapper<Supplier, String> {
+
+    List<Supplier> findPageBreakByCondition(BaseConditionVO vo, RowBounds rb);
+
+    Integer findNumberByCondition(BaseConditionVO vo);
+
+    void updateStatus(@Param("code") String ccyCode, @Param("deleted") boolean b, @Param("modUsr") String modUsr, @Param("modDate") Date date);
+
+    Supplier getSupplierByCode(@Param("code") String code);
+
+    List<Supplier> findAllSuppliers();
+}
