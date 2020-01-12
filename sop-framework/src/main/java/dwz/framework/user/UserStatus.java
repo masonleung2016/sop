71251e028eec275b9fd15d49f50df6b813fb2b4d
masonleung2016 <masonleung@163.com>
2020-01-12 11:36:35 +0800

    Create UserStatus.java

---
+++ sop-framework/src/main/java/dwz/framework/user/UserStatus.java
@@ -0,0 +1,11 @@
+package dwz.framework.user;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 17:16
+ * @Package: dwz.framework.user
+ */
+
+public enum UserStatus {
+    PENDING, ACTIVE, INACTIVE, DELETED
+}
