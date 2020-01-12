c28780c2b3fc4d708d8d646defcad7be14f1f3f5
masonleung2016 <masonleung@163.com>
2020-01-12 11:10:55 +0800

    Create IdSequenceDao.java

---
+++ sop-framework/src/main/java/dwz/dal/common/sequence/dao/IdSequenceDao.java
@@ -0,0 +1,12 @@
+package dwz.dal.common.sequence.dao;
+
+import dwz.dal.BaseMapper;
+
+/**
+ * @Author: LCF
+ * @Date: 2020/1/8 16:18
+ * @Package: dwz.dal.common.sequence.dao
+ */
+public interface IdSequenceDao extends BaseMapper<DalSequenceDO, String> {
+
+}
