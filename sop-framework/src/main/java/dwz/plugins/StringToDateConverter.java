2f936216386661b3db481b2ecf54e03630213fe9
masonleung2016 <masonleung@163.com>
2020-01-30 23:22:02 +0800

    Update StringToDateConverter.java

--- sop-framework/src/main/java/dwz/plugins/StringToDateConverter.java
+++ sop-framework/src/main/java/dwz/plugins/StringToDateConverter.java
@@ -16,6 +16,7 @@ import org.springframework.core.convert.converter.Converter;
 public class StringToDateConverter implements Converter<String, Date> {
 
     public Date convert(String source) {
+        
         DateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date date = null;
         try {
