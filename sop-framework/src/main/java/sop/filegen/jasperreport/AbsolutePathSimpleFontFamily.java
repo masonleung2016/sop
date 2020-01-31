ae4582c924133654a33d6c75e52a4acdfd2bb428
masonleung2016 <masonleung@163.com>
2020-01-31 12:05:09 +0800

    Update AbsolutePathSimpleFontFamily.java

--- sop-framework/src/main/java/sop/filegen/jasperreport/AbsolutePathSimpleFontFamily.java
+++ sop-framework/src/main/java/sop/filegen/jasperreport/AbsolutePathSimpleFontFamily.java
@@ -18,23 +18,34 @@ import net.sf.jasperreports.engine.util.JRDataUtils;
 
 public class AbsolutePathSimpleFontFamily implements FontFamily {
 
-    /**
-     *
-     */
     private JasperReportsContext jasperReportsContext;
+
     private String name;
+
     private FontFace normalFace;
+
     private FontFace boldFace;
+
     private FontFace italicFace;
+
     private FontFace boldItalicFace;
+
     private String normalPdfFont;
+
     private String boldPdfFont;
+
     private String italicPdfFont;
+
     private String boldItalicPdfFont;
+
     private String pdfEncoding;
+
     private Boolean isPdfEmbedded;
+
     private String defaultExportFont;
+    
     private Map<String, String> exportFonts;
+    
     private Set<String> locales;
 
     public AbsolutePathSimpleFontFamily() {
@@ -45,214 +56,123 @@ public class AbsolutePathSimpleFontFamily implements FontFamily {
         this.jasperReportsContext = jasperReportsContext;
     }
 
-
-    /**
-     *
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     *
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     *
-     */
     public void setNormal(String normal) {
         normalFace = AbsolutePathSimpleFontFace.getInstance(jasperReportsContext, normal);
     }
 
-    /**
-     *
-     */
     public void setBold(String bold) {
         boldFace = AbsolutePathSimpleFontFace.getInstance(jasperReportsContext, bold);
     }
 
-    /**
-     *
-     */
     public void setItalic(String italic) {
         italicFace = AbsolutePathSimpleFontFace.getInstance(jasperReportsContext, italic);
     }
 
-    /**
-     *
-     */
     public void setBoldItalic(String boldItalic) {
         boldItalicFace = AbsolutePathSimpleFontFace.getInstance(jasperReportsContext, boldItalic);
     }
 
-    /**
-     *
-     */
     public FontFace getNormalFace() {
         return normalFace;
     }
 
-    /**
-     *
-     */
     public FontFace getBoldFace() {
         return boldFace;
     }
 
-    /**
-     *
-     */
     public FontFace getItalicFace() {
         return italicFace;
     }
 
-    /**
-     *
-     */
     public FontFace getBoldItalicFace() {
         return boldItalicFace;
     }
 
-    /**
-     *
-     */
     public String getNormalPdfFont() {
         return normalPdfFont;
     }
 
-    /**
-     *
-     */
     public void setNormalPdfFont(String normalPdfFont) {
         this.normalPdfFont = normalPdfFont;
     }
 
-    /**
-     *
-     */
     public String getBoldPdfFont() {
         return boldPdfFont;
     }
 
-    /**
-     *
-     */
     public void setBoldPdfFont(String boldPdfFont) {
         this.boldPdfFont = boldPdfFont;
     }
 
-    /**
-     *
-     */
     public String getItalicPdfFont() {
         return italicPdfFont;
     }
 
-    /**
-     *
-     */
     public void setItalicPdfFont(String italicPdfFont) {
         this.italicPdfFont = italicPdfFont;
     }
 
-    /**
-     *
-     */
     public String getBoldItalicPdfFont() {
         return boldItalicPdfFont;
     }
 
-    /**
-     *
-     */
     public void setBoldItalicPdfFont(String boldItalicPdfFont) {
         this.boldItalicPdfFont = boldItalicPdfFont;
     }
 
-    /**
-     *
-     */
     public String getPdfEncoding() {
         return pdfEncoding;
     }
 
-    /**
-     *
-     */
     public void setPdfEncoding(String pdfEncoding) {
         this.pdfEncoding = pdfEncoding;
     }
 
-    /**
-     *
-     */
     public Boolean isPdfEmbedded() {
         return isPdfEmbedded;
     }
 
-    /**
-     *
-     */
     public void setPdfEmbedded(Boolean isPdfEmbedded) {
         this.isPdfEmbedded = isPdfEmbedded;
     }
 
-    /**
-     *
-     */
     public String getDefaultExportFont() {
         return defaultExportFont;
     }
 
-    /**
-     *
-     */
     public void setDefaultExportFont(String defaultExportFont) {
         this.defaultExportFont = defaultExportFont;
     }
 
-    /**
-     *
-     */
     public Map<String, String> getExportFonts() {
         return exportFonts;
     }
 
-    /**
-     *
-     */
     public void setExportFonts(Map<String, String> exportFonts) {
         this.exportFonts = exportFonts;
     }
 
-    /**
-     *
-     */
     public String getExportFont(String key) {
         String exportFont = exportFonts == null ? null : (String) exportFonts.get(key);
         return exportFont == null ? defaultExportFont : exportFont;
     }
 
-    /**
-     *
-     */
     public Set<String> getLocales() {
         return locales;
     }
 
-    /**
-     *
-     */
     public void setLocales(Set<String> locales) {
         this.locales = locales;
     }
 
-    /**
-     *
-     */
     public boolean supportsLocale(Locale locale) {
         return locales == null || locales.isEmpty() || locales.contains(JRDataUtils.getLocaleCode(locale));
     }
@@ -262,6 +182,4 @@ public class AbsolutePathSimpleFontFamily implements FontFamily {
         // TODO Auto-generated method stub
         return false;
     }
-
-
 }
