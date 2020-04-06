f08d195880c1c13b2c2936748ddf562bff7345f5
masonleung2016 <masonleung@163.com>
2020-04-06 11:30:53 +0800

    Update PackingDetailServiceMgr.java

--- sop-framework/src/main/java/sop/services/PackingDetailServiceMgr.java
+++ sop-framework/src/main/java/sop/services/PackingDetailServiceMgr.java
@@ -14,7 +14,6 @@ import sop.persistence.beans.PackingDetail;
  * @Package: sop.services
  */
 
-
 public interface PackingDetailServiceMgr extends BusinessObjectServiceMgr {
     String SERVICE_NAME = "packingDetailServiceMgr";
 
