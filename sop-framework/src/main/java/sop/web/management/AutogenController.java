05dbc20847b57b6d1aa04b812800a91cfa5ea810
masonleung2016 <masonleung@163.com>
2020-05-02 14:17:01 +0800

    Update AutogenController.java

--- sop-framework/src/main/java/sop/web/management/AutogenController.java
+++ sop-framework/src/main/java/sop/web/management/AutogenController.java
@@ -43,72 +43,117 @@ public class AutogenController extends BaseController {
 
     @RequestMapping("/list")
     public String list(BaseConditionVO vo, Model model) {
+        
         List<AutogenVo> autogenVoList = autogenMgr.getAutogenListByCondition(vo);
+        
         autogenMgr.getAutogenListNum(vo);
+        
         model.addAttribute("totalCount", vo.getTotalCount());
+        
         model.addAttribute("pageSize", vo.getPageSize());
+        
         model.addAttribute("autogenVoList", autogenVoList);
+        
         model.addAttribute("vo", vo);
+        
         return "/management/autogen/list";
     }
 
     @RequestMapping("/add")
     public String add(Model model, HttpServletRequest request) {
+        
         List<CompanyVo> autogenCompanyList = autogenMgr.getAllCompany();
+        
         model.addAttribute("autogenCompanyList", autogenCompanyList);
+        
         return "/management/autogen/add";
     }
 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
     public ModelAndView insert(Autogen autogen, HttpServletRequest request) {
+        
         Checker checker = autogenMgr.checkAutogen(autogen, false);
+        
         if (checker.isSuccess()) {
+            
             User currentUser = (User) request.getSession().getAttribute(Constants.AUTHENTICATION_KEY);
+            
             if (currentUser != null) {
+                
                 autogen.setCrtUsr(currentUser.getUsrId());
+                
                 autogen.setModUsr(currentUser.getUsrId());
             }
             try {
                 if (autogenMgr.addAutogen(autogen)) {
+                    
                     return ajaxDoneSuccess(getMessage("msg.operation.success"));
+                    
                 } else {
+                    
                     return ajaxDoneError(getMessage("msg.operation.failure"));
+                    
                 }
             } catch (ServiceException e) {
+                
                 return ajaxDoneError(e.getMessage());
+                
             }
+            
         } else {
+            
             return ajaxDoneError(checker.getReturnStr());
+            
         }
     }
 
     @RequestMapping("/edit/{key}")
     public String edit(@PathVariable("key") String key, Model model) {
+        
         //String key1 = Base64.decode(key, "utf-8");
         List<CompanyVo> autogenCompanyList = autogenMgr.getAllCompany();
+        
         model.addAttribute("autogenCompanyList", autogenCompanyList);
+        
         String[] keys = StringUtils.split(key, "|");
+        
         Autogen autogen = autogenMgr.getAutogen(keys[0], keys[1]);
+        
         model.addAttribute("autogen", autogen);
+        
         return "/management/autogen/edit";
     }
 
     @RequestMapping("/update")
     public ModelAndView update(Autogen autogen, HttpServletRequest request) {
+        
         Checker checker = autogenMgr.checkAutogen(autogen, true);
+        
         if (checker.isSuccess()) {
+            
             try {
+                
                 User currentUser = (User) request.getSession().getAttribute(Constants.AUTHENTICATION_KEY);
+                
                 if (currentUser != null) {
+                    
                     autogen.setCrtUsr(currentUser.getUsrId());
+                    
                     autogen.setModUsr(currentUser.getUsrId());
+                    
                 }
+                
                 autogenMgr.updateAutogen(autogen);
+                
             } catch (ServiceException e) {
+                
                 e.printStackTrace();
+                
                 return ajaxDoneError(e.getMessage());
             }
+            
             return ajaxDoneSuccess(getMessage("msg.operation.success"));
+            
         } else {
             return ajaxDoneError(checker.getReturnStr());
         }
@@ -116,15 +161,23 @@ public class AutogenController extends BaseController {
 
     @RequestMapping("/delete/{key}")
     public ModelAndView delete(@PathVariable("key") String key) {
+        
         try {
+            
             //String key1 = Base64.decode(key, "utf-8");
             String[] keys = StringUtils.split(key, "|");
+            
             if (autogenMgr.deleteAutogen(keys[0], keys[1])) {
+                
                 return ajaxDoneSuccess(getMessage("msg.operation.success"));
+                
             } else {
+                
                 return ajaxDoneError(getMessage("msg.operation.failure"));
+                
             }
         } catch (ServiceException e) {
+            
             return ajaxDoneError(e.toString());
         }
     }
