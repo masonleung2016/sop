313e0792e09b825f234be12489a0f16ba4f71317
masonleung2016 <masonleung@163.com>
2020-05-02 15:52:42 +0800

    Update ChargeController.java

--- sop-framework/src/main/java/sop/web/management/ChargeController.java
+++ sop-framework/src/main/java/sop/web/management/ChargeController.java
@@ -31,7 +31,6 @@ import sop.vo.ChargeVo;
  * @Package: sop.web.management
  */
 
-
 @Controller("management.chargeController")
 @RequestMapping(value = "/management/charge")
 //@SessionAttributes({"currentCharge"})
@@ -42,53 +41,89 @@ public class ChargeController extends BaseController {
 
     @RequestMapping("/list")
     public String list(BaseConditionVO vo, Model model) {
+        
         List<ChargeVo> chargeVoList = chargeMgr.getChargeListByCondition(vo);
+        
         chargeMgr.getChargeListNum(vo);
+        
         model.addAttribute("totalCount", vo.getTotalCount());
+        
         model.addAttribute("pageSize", vo.getPageSize());
+        
         model.addAttribute("chargeVoList", chargeVoList);
+        
         model.addAttribute("vo", vo);
+        
         return "/management/charge/list";
     }
 
     @RequestMapping("/add")
     public String add(Model model, HttpServletRequest request) {
+        
         return "/management/charge/add";
+        
     }
 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
     public ModelAndView insert(Charge charge, HttpServletRequest request) {
+        
         Checker checker = chargeMgr.checkCharge(charge);
+        
         if (checker.isSuccess()) {
+            
             Checker checkerDecimal = chargeMgr.checkChargeDecimal(charge);
+            
             if (checkerDecimal.isSuccess()) {
+                
                 User currentUser = (User) request.getSession().getAttribute(Constants.AUTHENTICATION_KEY);
+                
                 if (currentUser != null) {
+                    
                     charge.setCrtUsr(currentUser.getUsrId());
+                    
                     charge.setModUsr(currentUser.getUsrId());
+                    
                 }
                 try {
+                    
                     if (chargeMgr.addCharge(charge)) {
+                        
                         return ajaxDoneSuccess(getMessage("msg.operation.success"));
+                        
                     } else {
+                        
                         return ajaxDoneError(getMessage("msg.operation.failure"));
+                        
                     }
+                    
                 } catch (ServiceException e) {
+                    
                     e.printStackTrace();
+                    
                     return ajaxDoneError(e.getMessage());
+                    
                 }
+                
             } else {
+                
                 return ajaxDoneError(checkerDecimal.getReturnStr());
+                
             }
+            
         } else {
+            
             return ajaxDoneError(checker.getReturnStr());
+            
         }
     }
 
     @RequestMapping("/edit/{fkChgCode}")
     public String edit(@PathVariable("fkChgCode") String fkChgCode, Model model, HttpServletRequest request) {
+        
         fkChgCode = Base64.decode(fkChgCode, "utf-8");
+        
         Charge currentCharge = chargeMgr.getChargeByFkChgCode(fkChgCode);
+        
         model.addAttribute("currentCharge", currentCharge);
 
         return "/management/charge/edit";
@@ -96,38 +131,67 @@ public class ChargeController extends BaseController {
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public ModelAndView update(@ModelAttribute("currentCharge") Charge currentCharge, HttpServletRequest request) {
+        
         Checker checker = chargeMgr.checkChargeDecimal(currentCharge);
+        
         if (checker.isSuccess()) {
+            
             User currentUser = (User) request.getSession().getAttribute(Constants.AUTHENTICATION_KEY);
+            
             if (currentUser != null) {
+                
                 currentCharge.setModUsr(currentUser.getUsrId());
+                
             }
+            
             try {
+                
                 if (chargeMgr.updateCharge(currentCharge)) {
+                    
                     return ajaxDoneSuccess(getMessage("msg.operation.success"));
+                    
                 } else {
+                    
                     return ajaxDoneError(getMessage("msg.operation.failure"));
+                    
                 }
+                
             } catch (ServiceException e) {
+                
                 e.printStackTrace();
+                
                 return ajaxDoneError(e.getMessage());
+                
             }
+            
         } else {
+            
             return ajaxDoneError(checker.getReturnStr());
+            
         }
     }
 
     @RequestMapping("/delete/{fkChgCode}")
     public ModelAndView delete(@PathVariable("fkChgCode") String fkChgCode, HttpServletRequest request) {
+        
         fkChgCode = Base64.decode(fkChgCode, "utf-8");
+        
         try {
+            
             if (chargeMgr.deleteCharge(fkChgCode)) {
+                
                 return ajaxDoneSuccess(getMessage("msg.operation.success"));
+                
             } else {
+                
                 return ajaxDoneError(getMessage("msg.operation.failure"));
+                
             }
+            
         } catch (ServiceException e) {
+            
             return ajaxDoneError(e.toString());
+            
         }
     }
 //	@RequestMapping(value = "/clearcurrentuom", method = RequestMethod.POST)
