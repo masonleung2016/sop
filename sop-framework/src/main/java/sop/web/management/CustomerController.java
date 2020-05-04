b57257998acf5afda32110c319ab40ea2f5806df
masonleung2016 <masonleung@163.com>
2020-05-04 09:51:16 +0800

    Update CustomerController.java

--- sop-framework/src/main/java/sop/web/management/CustomerController.java
+++ sop-framework/src/main/java/sop/web/management/CustomerController.java
@@ -47,67 +47,114 @@ public class CustomerController extends BaseController {
 
     @RequestMapping("/list")
     public String index(BaseConditionVO vo, Model model) {
+        
         List<CustomerVo> customerList = customerMgr.searchCustomer(vo);
+        
         Integer totalCount = customerMgr.searchCustomerNum(vo);
 
         vo.setTotalCount(totalCount);
+        
         model.addAttribute("totalCount", totalCount);
+        
         model.addAttribute("pageSize", vo.getPageSize());
+        
         model.addAttribute("vo", vo);
+        
         model.addAttribute("customerList", customerList);
+        
         return "/management/customer/list";
     }
 
     @RequestMapping("/add")
     public String add(Model model) {
+        
         CustomerComboVo customerComboVo = customerMgr.getCustomerComboVo();
+        
         model.addAttribute("customerComboVo", customerComboVo);
+        
         Customer customer = new Customer();
+        
         model.addAttribute("customer", customer);
+        
         return "/management/customer/add";
+        
     }
 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
     public ModelAndView insert(@ModelAttribute("customer") Customer customer, HttpServletRequest request) {
+        
         Checker checker = customerMgr.checkCustomer(customer);
+        
         if (checker.isSuccess()) {
+            
             Checker checkerDecimal = customerMgr.checkDecimal(customer);
+            
             if (checker.isSuccess()) {
+                
                 try {
+                    
                     User user = (User) request.getSession().getAttribute(Constants.AUTHENTICATION_KEY);
+                    
                     if (user != null) {
+                        
                         customer.setCrtUsr(user.getUsrId());
+                        
                         customer.setModUsr(user.getUsrId());
+                        
                     }
+                    
                     customerMgr.addCustomer(customer);
+                    
                 } catch (ServiceException e) {
+                    
                     e.printStackTrace();
+                    
                     return ajaxDoneError("Code 重复");
+                    
                 }
+                
                 return ajaxDoneSuccess(getMessage("msg.operation.success"));
+                
             } else {
+                
                 return ajaxDoneError(checkerDecimal.getReturnStr());
+                
             }
+            
         } else {
+            
             return ajaxDoneError(checker.getReturnStr());
+            
         }
+        
     }
 
     @RequestMapping("/edit/{fkCuCode}")
     public String edit(@PathVariable("fkCuCode") String fkCuCode, Model model) {
+        
         fkCuCode = Base64.decode(fkCuCode, "utf-8");
+        
         CustomerComboVo customerComboVo = customerMgr.getCustomerComboVo();
+        
         Customer customer = customerMgr.getCustomerByCode(fkCuCode);
+        
         model.addAttribute("customerComboVo", customerComboVo);
+        
         model.addAttribute("customer", customer);
+        
         return "/management/customer/edit";
     }
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
+    
     public ModelAndView update(@ModelAttribute("customer") Customer customer, HttpServletRequest request) {
+        
         Checker checker = customerMgr.checkDecimal(customer);
+        
         if (checker.isSuccess()) {
+            
             try {
+                
                 dwz.framework.user.User currentUser = (dwz.framework.user.User) request.getSession().getAttribute(dwz.framework.config.Constants.AUTHENTICATION_KEY);
                 if (currentUser != null) {
                     customer.setModUsr(currentUser.getUsrId());
